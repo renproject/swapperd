a2b5de8b1da59627b6c025041473c12245c0bc29
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-06-12 11:17:40 +1000

    Add network service interface

--- services/network/network.go
+++ services/network/network.go
@@ -1,6 +1,6 @@
 package network
 
 type Network interface {
-	Send([32]byte, []byte) error
-	Recieve([32]byte) ([]byte, error)
+	RecieveSwapDetails([32]byte) ([]byte, error)
+	SendSwapDetails([32]byte, []byte) error
 }
