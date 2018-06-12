6b9649e44a4bcbc57dcf663e53be082b1268184a
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-06-12 11:17:52 +1000

    Add order services interface

--- services/order/order.go
+++ services/order/order.go
@@ -3,8 +3,8 @@ package order
 import "math/big"
 
 type Order interface {
-	MyOrderID() [32]byte
-	TradingOrderID() [32]byte
+	PersonalOrderID() [32]byte
+	ForeignOrderID() [32]byte
 	SendValue() *big.Int
 	RecieveValue() *big.Int
 	SendCurrency() string
