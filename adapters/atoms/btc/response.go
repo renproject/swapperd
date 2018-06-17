9f8e15d0499a0f203f5aac0fc1c0851df71d6d61
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-06-17 21:32:45 +1000

    Updated bitcoin atom responder

--- adapters/atoms/btc/response.go
+++ adapters/atoms/btc/response.go
@@ -8,19 +8,20 @@ import (
 
 	bindings "github.com/republicprotocol/atom-go/adapters/bindings/btc"
 	"github.com/republicprotocol/atom-go/adapters/clients/btc"
-	"github.com/republicprotocol/atom-go/services/atom"
+	"github.com/republicprotocol/atom-go/services/swap"
 )
 
-type BitcoinResponseAtom struct {
+// BitcoinAtomResponder is a struct for Bitcoin AtomResponder
+type BitcoinAtomResponder struct {
 	personalAddress string
 	foreignAddress  string
-	connection      btc.Connection
+	connection      btc.Conn
 	data            BitcoinData
 }
 
-// NewBitcoinResponseAtom returns a new Bitcoin ResponseAtom instance
-func NewBitcoinResponseAtom(connection btc.Connection, personalAddress, foreignAddress string) atom.ResponseAtom {
-	return &BitcoinResponseAtom{
+// NewBitcoinAtomResponder returns a new Bitcoin AtomResponder instance
+func NewBitcoinAtomResponder(connection btc.Conn, personalAddress, foreignAddress string) swap.AtomResponder {
+	return &BitcoinAtomResponder{
 		personalAddress: personalAddress,
 		foreignAddress:  foreignAddress,
 		connection:      connection,
@@ -28,7 +29,7 @@ func NewBitcoinResponseAtom(connection btc.Connection, personalAddress, foreignA
 }
 
 // Redeem an Atom swap by calling a function on Bitcoin
-func (btcAtom *BitcoinResponseAtom) Redeem(secret [32]byte) error {
+func (btcAtom *BitcoinAtomResponder) Redeem(secret [32]byte) error {
 
 	result, err := bindings.Redeem(btcAtom.connection, btcAtom.personalAddress, btcAtom.data.Contract, btcAtom.data.ContractTx, secret)
 	if err != nil {
@@ -40,7 +41,7 @@ func (btcAtom *BitcoinResponseAtom) Redeem(secret [32]byte) error {
 }
 
 // Audit an Atom swap by calling a function on Bitcoin
-func (btcAtom *BitcoinResponseAtom) Audit(hash [32]byte, to []byte, value *big.Int, expiry int64) error {
+func (btcAtom *BitcoinAtomResponder) Audit(hash [32]byte, to []byte, value *big.Int, expiry int64) error {
 	result, err := bindings.Audit(btcAtom.connection, btcAtom.data.Contract, btcAtom.data.ContractTx)
 	if err != nil {
 		return err
@@ -66,22 +67,22 @@ func (btcAtom *BitcoinResponseAtom) Audit(hash [32]byte, to []byte, value *big.I
 }
 
 // Serialize serializes the atom details into a bytes array
-func (btcAtom *BitcoinResponseAtom) Serialize() ([]byte, error) {
+func (btcAtom *BitcoinAtomResponder) Serialize() ([]byte, error) {
 	b, err := json.Marshal(btcAtom.data)
 	return b, err
 }
 
 // Deserialize deserializes the atom details from a bytes array
-func (btcAtom *BitcoinResponseAtom) Deserialize(b []byte) error {
+func (btcAtom *BitcoinAtomResponder) Deserialize(b []byte) error {
 	return json.Unmarshal(b, &btcAtom.data)
 }
 
 // PriorityCode returns the priority code of the currency.
-func (btcAtom *BitcoinResponseAtom) PriorityCode() int64 {
+func (btcAtom *BitcoinAtomResponder) PriorityCode() int64 {
 	return 0
 }
 
 // GetSecretHash returns the Secret Hash of the atom.
-func (btcAtom *BitcoinResponseAtom) GetSecretHash() [32]byte {
+func (btcAtom *BitcoinAtomResponder) GetSecretHash() [32]byte {
 	return btcAtom.data.HashLock
 }
