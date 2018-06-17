ea704d8cc7112472123c65c56ce8bb2a2bd34184
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-06-17 21:40:28 +1000

    Add 65 byte function to utils

--- utils/bytes.go
+++ utils/bytes.go
@@ -13,3 +13,15 @@ func ToBytes32(b []byte) ([32]byte, error) {
 	}
 	return bytes32, nil
 }
+
+// ToBytes65 converts dynamic byte array of size 65 to static byte array of size 65
+func ToBytes65(b []byte) ([65]byte, error) {
+	bytes65 := [65]byte{}
+	if len(b) != 65 {
+		return bytes65, errors.New("Length mismatch")
+	}
+	for i := range b {
+		bytes65[i] = b[i]
+	}
+	return bytes65, nil
+}
