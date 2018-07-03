5bcc1a7db8a4ffddf1cf3be8e3b3bc16ff873e8b
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-07-03 12:39:57 +1000

    Add mock ethereum wallet

---
+++ adapters/wallet/mock/wallet.go
@@ -0,0 +1,36 @@
+package mock
+
+import (
+	"math/big"
+	"sync"
+
+	"github.com/republicprotocol/atom-go/domains/match"
+	"github.com/republicprotocol/atom-go/services/watch"
+)
+
+type MockWallet struct {
+	mu      *sync.RWMutex
+	matches map[[32]byte]match.Match
+}
+
+func NewMockWallet() watch.Wallet {
+	matches := make(map[[32]byte]match.Match)
+	return &MockWallet{
+		mu:      &sync.RWMutex{},
+		matches: matches,
+	}
+}
+
+func (wallet *MockWallet) SetMatch(m match.Match) error {
+	wallet.mu.Lock()
+	defer wallet.mu.Unlock()
+	wallet.matches[m.PersonalOrderID()] = match.NewMatch(m.PersonalOrderID(), m.ForeignOrderID(), m.SendValue(), m.RecieveValue().Sub(m.RecieveValue(), big.NewInt(10000)), m.SendCurrency(), m.RecieveCurrency())
+	wallet.matches[m.ForeignOrderID()] = match.NewMatch(m.ForeignOrderID(), m.PersonalOrderID(), m.RecieveValue(), m.SendValue().Sub(m.SendValue(), big.NewInt(10000)), m.RecieveCurrency(), m.SendCurrency())
+	return nil
+}
+
+func (wallet *MockWallet) GetMatch(orderID [32]byte) (match.Match, error) {
+	wallet.mu.RLock()
+	defer wallet.mu.RUnlock()
+	return wallet.matches[orderID], nil
+}
