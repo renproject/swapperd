8b0040bc8d93e16af2d983edc8250031605f1378
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-07-03 12:34:19 +1000

    Add Bitcoin Network Config

--- adapters/config/btc.go
+++ adapters/config/btc.go
@@ -1,52 +1,21 @@
 package config
 
-import "errors"
-
-var (
-	ErrUnknownChain = errors.New("unknown chain")
-)
-
-type BitcoinNetworkConfig struct {
+type BitcoinConfig struct {
 	Chain    string `json:"chain"`
 	User     string `json:"username"`
 	Password string `json:"password"`
 	URL      string `json:"url"`
 }
 
-type BitcoinConfig struct {
-	Regtest BitcoinNetworkConfig `json:"regtest"`
-	Testnet BitcoinNetworkConfig `json:"testnet"`
-	Mainnet BitcoinNetworkConfig `json:"mainnet"`
-}
-
-func (config *Config) GetBitcoinConfig(chain string) (BitcoinNetworkConfig, error) {
+func (config *Config) GetBitcoinConfig() BitcoinConfig {
 	config.mu.RLock()
 	defer config.mu.RUnlock()
-	switch chain {
-	case "regtest":
-		return config.Bitcoin.Regtest, nil
-	case "testnet":
-		return config.Bitcoin.Testnet, nil
-	case "mainnet":
-		return config.Bitcoin.Mainnet, nil
-	default:
-		return BitcoinNetworkConfig{}, ErrUnknownChain
-	}
+	return config.Bitcoin
 }
 
-func (config *Config) SetBitcoinConfig(chain string, bitcoinConfig BitcoinNetworkConfig) error {
+func (config *Config) SetBitcoinConfig(bitcoinConfig BitcoinConfig) {
 	config.mu.Lock()
 	defer config.mu.Unlock()
-	switch chain {
-	case "regtest":
-		config.Bitcoin.Regtest = bitcoinConfig
-	case "testnet":
-		config.Bitcoin.Testnet = bitcoinConfig
-	case "mainnet":
-		config.Bitcoin.Mainnet = bitcoinConfig
-	default:
-		return ErrUnknownChain
-	}
+	config.Bitcoin = bitcoinConfig
 	config.Update()
-	return nil
 }
