a206622d320a44e4b9f095bfb8a7519623af74aa
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-07-03 12:34:34 +1000

    Add ethereum network config

--- adapters/config/eth.go
+++ adapters/config/eth.go
@@ -1,7 +1,7 @@
 package config
 
 // Config are the parameters required to create an ethereum client
-type EthereumNetworkConfig struct {
+type EthereumConfig struct {
 	Chain          string `json:"chain"`
 	URL            string `json:"url"`
 	AtomAddress    string `json:"atom_address"`
@@ -10,45 +10,15 @@ type EthereumNetworkConfig struct {
 	WalletAddress  string `json:"wallet_address"`
 }
 
-type EthereumConfig struct {
-	Ganache EthereumNetworkConfig `json:"ganache"`
-	Ropsten EthereumNetworkConfig `json:"ropsten"`
-	Kovan   EthereumNetworkConfig `json:"kovan"`
-	Mainnet EthereumNetworkConfig `json:"mainnet"`
-}
-
-func (config *Config) GetEthereumConfig(chain string) (EthereumNetworkConfig, error) {
+func (config *Config) GetEthereumConfig() EthereumConfig {
 	config.mu.RLock()
 	defer config.mu.RUnlock()
-	switch chain {
-	case "ganache":
-		return config.Ethereum.Ganache, nil
-	case "ropsten":
-		return config.Ethereum.Ropsten, nil
-	case "kovan":
-		return config.Ethereum.Kovan, nil
-	case "mainnet":
-		return config.Ethereum.Mainnet, nil
-	default:
-		return EthereumNetworkConfig{}, ErrUnknownChain
-	}
+	return config.Ethereum
 }
 
-func (config *Config) SetEthereumConfig(chain string, ethereumConfig EthereumNetworkConfig) error {
+func (config *Config) SetEthereumConfig(ethereumConfig EthereumConfig) {
 	config.mu.Lock()
 	defer config.mu.Unlock()
-	switch chain {
-	case "ganache":
-		config.Ethereum.Ganache = ethereumConfig
-	case "ropsten":
-		config.Ethereum.Ropsten = ethereumConfig
-	case "kovan":
-		config.Ethereum.Kovan = ethereumConfig
-	case "mainnet":
-		config.Ethereum.Mainnet = ethereumConfig
-	default:
-		return ErrUnknownChain
-	}
+	config.Ethereum = ethereumConfig
 	config.Update()
-	return nil
 }
