3c2f75bd8fe4e9e802b7fc445daf6a547859ce0e
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-07-05 01:03:48 +1000

    Updated the atom service

--- services/swap/atom.go
+++ services/swap/atom.go
@@ -2,21 +2,15 @@ package swap
 
 import "math/big"
 
-type AtomRequester interface {
+type Atom interface {
 	Initiate(to []byte, hash [32]byte, value *big.Int, expiry int64) error
 	Refund() error
 	AuditSecret() (secret [32]byte, err error)
-	Serialize() ([]byte, error)
-	Deserialize([]byte) error
-	PriorityCode() int64
-	From() []byte
-}
-
-type AtomResponder interface {
 	Redeem(secret [32]byte) error
-	GetSecretHash() [32]byte
 	Audit(hash [32]byte, to []byte, value *big.Int, delay int64) error
+	GetSecretHash() [32]byte
 	Serialize() ([]byte, error)
 	Deserialize([]byte) error
-	PriorityCode() int64
+	PriorityCode() uint32
+	From() []byte
 }
