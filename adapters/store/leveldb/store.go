5c36589db3a0f2e3ad7073aa4ccfb55208028949
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-07-05 01:01:59 +1000

    Add leveldb store implementaion

---
+++ adapters/store/leveldb/store.go
@@ -0,0 +1,32 @@
+package leveldb
+
+import (
+	"github.com/republicprotocol/atom-go/services/store"
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+type ldbStore struct {
+	path string
+}
+
+func NewLDBStore(path string) store.Store {
+	return &ldbStore{
+		path: path,
+	}
+}
+
+func (ldb *ldbStore) Read(key []byte) ([]byte, error) {
+	db, err := leveldb.OpenFile(ldb.path, nil)
+	if err != nil {
+		return []byte{}, err
+	}
+	return db.Get(key, nil)
+}
+
+func (ldb *ldbStore) Write(key []byte, value []byte) error {
+	db, err := leveldb.OpenFile(ldb.path, nil)
+	if err != nil {
+		return err
+	}
+	return db.Put(key, value, nil)
+}
