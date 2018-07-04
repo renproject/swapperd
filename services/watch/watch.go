aa4ff169d2c921d9b6607836e828533512098d25
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-07-05 01:08:07 +1000

    Updated watch to work with the new changes

--- services/watch/watch.go
+++ services/watch/watch.go
@@ -4,32 +4,44 @@ import (
 	"github.com/republicprotocol/atom-go/services/swap"
 )
 
-type Watch struct {
+type watch struct {
 	network swap.Network
 	info    swap.Info
-	reqAtom swap.AtomRequester
-	resAtom swap.AtomResponder
+	reqAtom swap.Atom
+	resAtom swap.Atom
 	wallet  Wallet
+	str     swap.SwapStore
 }
 
-func NewWatch(network swap.Network, info swap.Info, wallet Wallet, reqAtom swap.AtomRequester, resAtom swap.AtomResponder) Watch {
-	return Watch{
+type Watch interface {
+	Run([32]byte) error
+	Status([32]byte) (string, error)
+}
+
+func NewWatch(network swap.Network, info swap.Info, wallet Wallet, reqAtom swap.Atom, resAtom swap.Atom, str swap.SwapStore) Watch {
+	return &watch{
 		network: network,
 		info:    info,
 		wallet:  wallet,
 		reqAtom: reqAtom,
 		resAtom: resAtom,
+		str:     str,
 	}
 }
 
 // Run runs the watch object on the given order id
-func (watch *Watch) Run(orderID [32]byte) error {
+func (watch *watch) Run(orderID [32]byte) error {
 	match, err := watch.wallet.GetMatch(orderID)
 	if err != nil {
 		return err
 	}
+
 	// watch.info.SetOwnerAddress()
-	atomicSwap := swap.NewSwap(watch.reqAtom, watch.resAtom, watch.info, match, watch.network)
+	atomicSwap := swap.NewSwap(watch.reqAtom, watch.resAtom, watch.info, match, watch.network, watch.str)
 	err = atomicSwap.Execute()
 	return err
+}
+
+func (watch *watch) Status(orderID [32]byte) (string, error) {
+	return watch.str.ReadStatus(orderID)
 }
