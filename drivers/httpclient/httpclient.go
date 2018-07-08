4bf3f82c5b1ae3e74af0f65e4e1567d869ce8721
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-07-09 03:44:15 +1000

    Add a demo http client driver

--- drivers/httpclient/httpclient.go
+++ drivers/httpclient/httpclient.go
@@ -2,41 +2,165 @@ package main
 
 import (
 	"crypto/rand"
-	"log"
+	"encoding/hex"
+	"fmt"
+	"os"
+	"sync"
+	"time"
 
+	"github.com/btcsuite/btcutil"
+
+	"github.com/ethereum/go-ethereum/common"
 	ethCrypto "github.com/ethereum/go-ethereum/crypto"
 
-	"github.com/republicprotocol/atom-go/adapters/http"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/crypto"
+	btcclient "github.com/republicprotocol/atom-go/adapters/clients/btc"
+	ethclient "github.com/republicprotocol/atom-go/adapters/clients/eth"
+	"github.com/republicprotocol/atom-go/adapters/config"
 	"github.com/republicprotocol/atom-go/adapters/keystore"
-	"github.com/republicprotocol/atom-go/utils"
+	"github.com/republicprotocol/atom-go/adapters/owner"
+	wal "github.com/republicprotocol/atom-go/adapters/wallet/eth"
+	"github.com/republicprotocol/atom-go/domains/match"
+	"github.com/republicprotocol/atom-go/drivers/btc/regtest"
+	"github.com/republicprotocol/atom-go/services/swap"
 )
 
 func main() {
-	keystr := keystore.NewKeystore("/Users/susruth/go/src/github.com/republicprotocol/atom-go/drivers/httpclient/keystore.json")
 
-	key, err := keystr.LoadKeypair("ethereum")
+	var aliceOrderID, bobOrderID [32]byte
+
+	var conf = os.Getenv("HOME") + "/go/src/github.com/republicprotocol/atom-go/secrets/local/configA.json"
+	var keyA = os.Getenv("HOME") + "/go/src/github.com/republicprotocol/atom-go/secrets/local/keystoreA.json"
+	var keyB = os.Getenv("HOME") + "/go/src/github.com/republicprotocol/atom-go/secrets/local/keystoreB.json"
+	var ownPath = os.Getenv("HOME") + "/go/src/github.com/republicprotocol/atom-go/secrets/owner.json"
+
+	ksA := keystore.NewKeystore(keyA)
+	ksB := keystore.NewKeystore(keyB)
+
+	own, err := owner.LoadOwner(ownPath)
 	if err != nil {
 		panic(err)
 	}
 
-	orderID := [32]byte{}
+	ownerECDSA, err := crypto.HexToECDSA(own.Ganache)
+	if err != nil {
+		panic(err)
+	}
+	owner := bind.NewKeyedTransactor(ownerECDSA)
 
-	_, err = rand.Read(orderID[:])
+	config, err := config.LoadConfig(conf)
 	if err != nil {
 		panic(err)
 	}
 
-	sig, err := ethCrypto.Sign(orderID[:], key)
+	ethConn, err := ethclient.Connect(config)
 	if err != nil {
 		panic(err)
 	}
 
-	sig65, err := utils.ToBytes65(sig)
+	btcConn, err := btcclient.Connect(config)
 	if err != nil {
 		panic(err)
 	}
 
-	log.Println("address", ethCrypto.PubkeyToAddress(key.PublicKey).String())
-	log.Println("order id", http.MarshalOrderID(orderID))
-	log.Println("sig 65", http.MarshalSignature(sig65))
+	wg := new(sync.WaitGroup)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err = regtest.Mine(btcConn)
+		if err != nil {
+			panic(err)
+		}
+	}()
+	time.Sleep(5 * time.Second)
+
+	err = deposit(ksA, ksB, owner, ethConn, btcConn)
+	if err != nil {
+		panic(err)
+	}
+
+	rand.Read(aliceOrderID[:])
+	rand.Read(bobOrderID[:])
+
+	aliceSendValue := big.NewInt(10000000)
+	bobSendValue := big.NewInt(10000000)
+
+	aliceCurrency := uint32(1)
+	bobCurrency := uint32(0)
+
+	atomMatch := match.NewMatch(aliceOrderID, bobOrderID, aliceSendValue, bobSendValue, aliceCurrency, bobCurrency)
+	mockWallet, err := wal.NewEthereumWallet(ethConn, *owner)
+	if err != nil {
+		panic(err)
+	}
+
+	err = mockWallet.SetMatch(atomMatch)
+	if err != nil {
+		panic(err)
+	}
+
+	messageAlice := append([]byte("Republic Protocol: open: "), aliceOrderID[:]...)
+	signatureDataAlice := ethCrypto.Keccak256([]byte(fmt.Sprintf("\x19Ethereum Signed Message:\n%d", len(messageAlice))), messageAlice)
+
+	sigAlice, err := ethCrypto.Sign(signatureDataAlice[:], ownerECDSA)
+	if err != nil {
+		panic(err)
+	}
+
+	messageBob := append([]byte("Republic Protocol: open: "), bobOrderID[:]...)
+	signatureDataBob := ethCrypto.Keccak256([]byte(fmt.Sprintf("\x19Ethereum Signed Message:\n%d", len(messageBob))), messageBob)
+
+	sigBob, err := ethCrypto.Sign(signatureDataBob[:], ownerECDSA)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Signer:", owner.From.String())
+	fmt.Println("Alice:", hex.EncodeToString(aliceOrderID[:]), "Signature:", hex.EncodeToString(sigAlice))
+	fmt.Println("Bob:", hex.EncodeToString(bobOrderID[:]), "Signature:", hex.EncodeToString(sigBob))
+	wg.Wait()
+}
+
+func deposit(ksA, ksB swap.Keystore, auth *bind.TransactOpts, ethConn ethclient.Conn, btcConn btcclient.Conn) error {
+	Alice, err := ksA.LoadKeys()
+	if err != nil {
+		return err
+	}
+	Bob, err := ksA.LoadKeys()
+	if err != nil {
+		return err
+	}
+
+	aEth := Alice[0]
+	bBtc := Bob[1]
+
+	aliceEth, err := aEth.GetAddress()
+	if err != nil {
+		return err
+	}
+
+	bobBtc, err := bBtc.GetAddress()
+	if err != nil {
+		return err
+	}
+
+	aliceAddr := common.BytesToAddress(aliceEth)
+	ethConn.Transfer(aliceAddr, auth, 1000000000000)
+
+	bobAddr, err := btcutil.DecodeAddress(string(bobBtc), btcConn.ChainParams)
+	if err != nil {
+		return err
+	}
+
+	bobVal, err := btcutil.NewAmount(0.05)
+	if err != nil {
+		return err
+	}
+
+	_, err = btcConn.Client.SendToAddress(bobAddr, bobVal)
+	return err
 }
