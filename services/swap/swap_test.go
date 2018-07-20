b1d5328bc13bd118379ed6ce2c3ba34412d38cdf
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-07-20 13:56:59 +1000

    Updated swap test

--- services/swap/swap_test.go
+++ services/swap/swap_test.go
@@ -4,11 +4,13 @@ import (
 	"crypto/ecdsa"
 	"crypto/rand"
 	"encoding/hex"
-	"fmt"
 	"math/big"
 	"os"
 	"sync"
 	"time"
+
+	"github.com/btcsuite/btcutil"
+	"github.com/republicprotocol/atom-go/services/store"
 
 	"github.com/republicprotocol/atom-go/domains/match"
 
@@ -25,6 +27,7 @@ import (
 	"github.com/republicprotocol/atom-go/adapters/atoms/eth"
 	"github.com/republicprotocol/atom-go/adapters/owner"
 
+	btcKey "github.com/republicprotocol/atom-go/adapters/key/btc"
 	ethKey "github.com/republicprotocol/atom-go/adapters/key/eth"
 
 	btcclient "github.com/republicprotocol/atom-go/adapters/clients/btc"
@@ -50,8 +53,7 @@ var _ = Describe("Ethereum - Bitcoin Atomic Swap", func() {
 		var aliceReceiveValue, bobReceiveValue *big.Int
 		var aliceCurrency, bobCurrency uint32
 		var alice, bob *ecdsa.PrivateKey
-		var aliceKey, bobKey Key
-		var aliceBitcoinAddress, bobBitcoinAddress string
+		var aliceEthKey, bobEthKey, aliceBtcKey, bobBtcKey Key
 		var swapID [32]byte
 
 		rand.Read(aliceOrderID[:])
@@ -62,7 +64,7 @@ var _ = Describe("Ethereum - Bitcoin Atomic Swap", func() {
 		aliceCurrency = 1
 		bobCurrency = 0
 
-		var confPath = os.Getenv("GOPATH") + "/src/github.com/republicprotocol/atom-go/secrets/config.json"
+		var confPath = os.Getenv("GOPATH") + "/src/github.com/republicprotocol/atom-go/secrets/local/configA.json"
 		config, err := config.LoadConfig(confPath)
 		Expect(err).ShouldNot(HaveOccurred())
 
@@ -81,17 +83,23 @@ var _ = Describe("Ethereum - Bitcoin Atomic Swap", func() {
 
 		alice, err = crypto.GenerateKey()
 		Expect(err).ShouldNot(HaveOccurred())
-		aliceKey, err = ethKey.NewEthereumKey(hex.EncodeToString(crypto.FromECDSA(alice)), "ganache")
+		aliceAuth := bind.NewKeyedTransactor(alice)
+		aliceEthKey, err = ethKey.NewEthereumKey(hex.EncodeToString(crypto.FromECDSA(alice)), "ganache")
+		Expect(err).ShouldNot(HaveOccurred())
+		aliceBtcKey, err = btcKey.NewBitcoinKey(hex.EncodeToString(crypto.FromECDSA(alice)), "regtest")
 		Expect(err).ShouldNot(HaveOccurred())
 
 		bob, err = crypto.GenerateKey()
 		Expect(err).ShouldNot(HaveOccurred())
-		bobKey, err = ethKey.NewEthereumKey(hex.EncodeToString(crypto.FromECDSA(bob)), "ganache")
+		bobAuth := bind.NewKeyedTransactor(bob)
+		bobEthKey, err = ethKey.NewEthereumKey(hex.EncodeToString(crypto.FromECDSA(bob)), "ganache")
+		Expect(err).ShouldNot(HaveOccurred())
+		bobBtcKey, err = btcKey.NewBitcoinKey(hex.EncodeToString(crypto.FromECDSA(bob)), "regtest")
 		Expect(err).ShouldNot(HaveOccurred())
 
-		aliceAddrBytes, err := aliceKey.GetAddress()
+		aliceAddrBytes, err := aliceEthKey.GetAddress()
 		Expect(err).ShouldNot(HaveOccurred())
-		bobAddrBytes, err := bobKey.GetAddress()
+		bobAddrBytes, err := bobEthKey.GetAddress()
 		Expect(err).ShouldNot(HaveOccurred())
 
 		err = ganache.Transfer(common.BytesToAddress(aliceAddrBytes), owner, 1000000000000000000)
@@ -116,14 +124,26 @@ var _ = Describe("Ethereum - Bitcoin Atomic Swap", func() {
 		}()
 		time.Sleep(5 * time.Second)
 
-		aliceAddr, err := regtest.GetAddressForAccount(connection, "alice")
+		_AliceWIF, err := aliceBtcKey.GetKeyString()
 		Expect(err).ShouldNot(HaveOccurred())
-		aliceBitcoinAddress = aliceAddr.EncodeAddress()
 
-		bobAddr, err := regtest.GetAddressForAccount(connection, "bob")
+		AliceWIF, err := btcutil.DecodeWIF(_AliceWIF)
 		Expect(err).ShouldNot(HaveOccurred())
-		bobBitcoinAddress = bobAddr.EncodeAddress()
-		Expect(err).Should(BeNil())
+
+		err = connection.Client.ImportPrivKeyLabel(AliceWIF, "alice")
+		Expect(err).ShouldNot(HaveOccurred())
+
+		_BobWIF, err := bobBtcKey.GetKeyString()
+		Expect(err).ShouldNot(HaveOccurred())
+
+		BobWIF, err := btcutil.DecodeWIF(_BobWIF)
+		Expect(err).ShouldNot(HaveOccurred())
+
+		err = connection.Client.ImportPrivKeyLabel(BobWIF, "bob")
+		Expect(err).ShouldNot(HaveOccurred())
+
+		_, err = regtest.GetAddressForAccount(connection, "bob")
+		Expect(err).ShouldNot(HaveOccurred())
 
 		aliceNet, err = net.NewEthereumNetwork(ganache, aliceAuth)
 		Expect(err).Should(BeNil())
@@ -131,57 +151,68 @@ var _ = Describe("Ethereum - Bitcoin Atomic Swap", func() {
 		bobNet, err = net.NewEthereumNetwork(ganache, bobAuth)
 		Expect(err).Should(BeNil())
 
-		aliceInfo, err = ax.NewEtereumAtomInfo(ganache, aliceAuth)
+		aliceInfo, err = ax.NewEthereumAtomInfo(ganache, aliceAuth)
 		Expect(err).Should(BeNil())
 
-		bobInfo, err = ax.NewEtereumAtomInfo(ganache, bobAuth)
+		bobInfo, err = ax.NewEthereumAtomInfo(ganache, bobAuth)
 		Expect(err).Should(BeNil())
 
 		aliceOrder = match.NewMatch(aliceOrderID, bobOrderID, aliceSendValue, aliceReceiveValue, aliceCurrency, bobCurrency)
 		bobOrder = match.NewMatch(bobOrderID, aliceOrderID, bobSendValue, bobReceiveValue, bobCurrency, aliceCurrency)
 
-		aliceInfo.SetOwnerAddress(aliceOrderID, []byte(aliceBitcoinAddress))
-		bobInfo.SetOwnerAddress(bobOrderID, bob.From.Bytes())
-
-		reqAlice, err := eth.NewEthereumAtom(ganache, alice)
+		aliceBtcAddrBytes, err := aliceBtcKey.GetAddress()
 		Expect(err).Should(BeNil())
 
-		reqBob := btc.NewBitcoinAtom(connection, bobBitcoinAddress)
-		resAlice := btc.NewBitcoinAtom(connection, aliceBitcoinAddress)
-
-		resBob, err := eth.NewEthereumAtom(ganache, bob)
+		bobEthAddrBytes, err := bobEthKey.GetAddress()
 		Expect(err).Should(BeNil())
 
-		aliceStr := NewSwapStore(leveldb.NewLDBStore("/db"))
-		bobStr := NewSwapStore(leveldb.NewLDBStore("/db"))
+		aliceInfo.SetOwnerAddress(aliceOrderID, aliceBtcAddrBytes)
+		bobInfo.SetOwnerAddress(bobOrderID, bobEthAddrBytes)
 
-		aliceSwap = NewSwap(reqAlice, resAlice, aliceInfo, aliceOrder, aliceNet, aliceStr)
-		bobSwap = NewSwap(reqBob, resBob, bobInfo, bobOrder, bobNet, bobStr)
+		reqAlice, err := eth.NewEthereumAtom(ganache, aliceEthKey, aliceOrderID)
+		Expect(err).Should(BeNil())
+
+		reqBob := btc.NewBitcoinAtom(connection, bobBtcKey, bobOrderID)
+		resAlice := btc.NewBitcoinAtom(connection, aliceBtcKey, bobOrderID)
+
+		resBob, err := eth.NewEthereumAtom(ganache, bobEthKey, aliceOrderID)
+		Expect(err).Should(BeNil())
+
+		aliceLDB, err := leveldb.NewLDBStore("/Users/susruth/go/src/github.com/republicprotocol/atom-go/temp/dbAlice")
+		Expect(err).Should(BeNil())
+
+		bobLDB, err := leveldb.NewLDBStore("/Users/susruth/go/src/github.com/republicprotocol/atom-go/temp/dbBob")
+		Expect(err).Should(BeNil())
+
+		aliceState := store.NewSwapState(aliceLDB)
+		bobState := store.NewSwapState(bobLDB)
+
+		aliceState.PutStatus(aliceOrderID, StatusInfoSubmitted)
+		bobState.PutStatus(bobOrderID, StatusInfoSubmitted)
+
+		aliceSwap = NewSwap(reqAlice, resAlice, aliceInfo, aliceOrder, aliceNet, aliceState)
+		bobSwap = NewSwap(reqBob, resBob, bobInfo, bobOrder, bobNet, bobState)
 	})
 
 	It("can do an eth - btc atomic swap", func() {
 		wg := &sync.WaitGroup{}
-
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
+			defer GinkgoRecover()
+
 			err := aliceSwap.Execute()
-			fmt.Println(err)
 			Expect(err).ShouldNot(HaveOccurred())
-
-			fmt.Println("Done 1")
 		}()
 
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
+			defer GinkgoRecover()
+
 			err := bobSwap.Execute()
-			fmt.Println(err)
 			Expect(err).ShouldNot(HaveOccurred())
-
-			fmt.Println("Done 2")
 		}()
-
 		wg.Wait()
 	})
 })
