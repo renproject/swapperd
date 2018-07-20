d27b8371517fef4dd36aa20b1b4ccdee2f7cebe9
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-07-20 13:56:47 +1000

    Add swap statuses as constants

---
+++ services/swap/swap_status.go
@@ -0,0 +1,18 @@
+package swap
+
+const (
+	StatusUnknown                     = "UNKNOWN"
+	StatusMatched                     = "MATCHED"
+	StatusInfoSubmitted               = "INFO_SUBMITTED"
+	StatusInitateDetailsAcquired      = "INITIATE_DETAILS_ACQUIRED"
+	StatusInitiated                   = "INITIATED"
+	StatusWaitingForCounterInitiation = "WAITING_FOR_COUNTER_INITIATION"
+	StatusRedeemDetailsAcquired       = "REDEEM_DETAILS_ACQUIRED"
+	StatusRedeemed                    = "REDEEMED"
+	StatusWaitingForCounterRedemption = "WAITING_FOR_COUNTER_REDEMPTION"
+	StatusRefunded                    = "REFUNDED"
+
+	StatusRecievedSwapDetails = "RECIEVED_SWAP_DETAILS"
+	StatusSentSwapDetails     = "SENT_SWAP_DETAILS"
+	StatusAudited             = "AUDITED"
+)
