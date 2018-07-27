21c289c0362b28e64d92f23f388593fb7c723916
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-07-27 11:00:34 +1000

    Updated the atom builder

--- adapters/atoms/atoms.go
+++ adapters/atoms/atoms.go
@@ -10,63 +10,70 @@ import (
 
 	"github.com/republicprotocol/atom-go/adapters/atoms/btc"
 	"github.com/republicprotocol/atom-go/adapters/atoms/eth"
-	"github.com/republicprotocol/atom-go/adapters/config"
+	"github.com/republicprotocol/atom-go/adapters/configs/keystore"
+	"github.com/republicprotocol/atom-go/adapters/configs/network"
 
-	btcClient "github.com/republicprotocol/atom-go/adapters/clients/btc"
-	ethClient "github.com/republicprotocol/atom-go/adapters/clients/eth"
+	"github.com/republicprotocol/atom-go/adapters/blockchain/binder"
+	btcClient "github.com/republicprotocol/atom-go/adapters/blockchain/clients/btc"
+	ethClient "github.com/republicprotocol/atom-go/adapters/blockchain/clients/eth"
 )
 
 type atomBuilder struct {
-	keys   []swap.Key
-	config config.Config
+	binder   binder.Binder
+	keystore keystore.Keystore
+	config   network.Config
 }
 
-func NewAtomBuilder(config config.Config, keys []swap.Key) swap.AtomBuilder {
-	return &atomBuilder{
-		keys:   keys,
-		config: config,
+type AtomBuilder interface {
+	BuildAtoms(state store.State, m match.Match) (swap.Atom, swap.Atom, error)
+}
+
+func NewAtomBuilder(config network.Config, keystore keystore.Keystore) (AtomBuilder, error) {
+	ethConn, err := ethClient.Connect(config)
+	if err != nil {
+		return nil, err
 	}
+	b, err := binder.NewBinder(keystore.EthereumKey.GetKey(), ethConn)
+	if err != nil {
+		return nil, err
+	}
+	return &atomBuilder{
+		binder:   b,
+		keystore: keystore,
+		config:   config,
+	}, nil
 }
 
 func (ab *atomBuilder) BuildAtoms(state store.State, m match.Match) (swap.Atom, swap.Atom, error) {
-
-	if len(ab.keys) != 2 {
-		return nil, nil, fmt.Errorf("This software does not support more than two keys at the moment")
-	}
-
 	var personalAtom, foreignAtom swap.Atom
 	var err error
 
-	if ab.keys[0].PriorityCode() == m.SendCurrency() {
-		personalAtom, err = buildAtom(ab.keys[0], ab.config, m.PersonalOrderID())
-		if err != nil {
-			return nil, nil, err
-		}
+	personalAtom, err = buildAtom(ab.binder, ab.keystore, ab.config, m.SendCurrency(), m.PersonalOrderID())
+	if err != nil {
+		return nil, nil, err
+	}
 
-		foreignAtom, err = buildAtom(ab.keys[1], ab.config, m.ForeignOrderID())
-		if err != nil {
-			return nil, nil, err
-		}
-	} else {
-		personalAtom, err = buildAtom(ab.keys[1], ab.config, m.PersonalOrderID())
-		if err != nil {
-			return nil, nil, err
-		}
-
-		foreignAtom, err = buildAtom(ab.keys[0], ab.config, m.ForeignOrderID())
-		if err != nil {
-			return nil, nil, err
-		}
+	foreignAtom, err = buildAtom(ab.binder, ab.keystore, ab.config, m.ReceiveCurrency(), m.ForeignOrderID())
+	if err != nil {
+		return nil, nil, err
 	}
 
 	if state.AtomExists(m.PersonalOrderID()) {
-		if err := personalAtom.Restore(state); err != nil {
+		details, err := state.AtomDetails(m.PersonalOrderID())
+		if err != nil {
+			return nil, nil, err
+		}
+		if err := personalAtom.Deserialize(details); err != nil {
 			return nil, nil, err
 		}
 	}
 
 	if state.AtomExists(m.ForeignOrderID()) {
-		if err := foreignAtom.Restore(state); err != nil {
+		details, err := state.AtomDetails(m.ForeignOrderID())
+		if err != nil {
+			return nil, nil, err
+		}
+		if err := foreignAtom.Deserialize(details); err != nil {
 			return nil, nil, err
 		}
 	}
@@ -74,20 +81,20 @@ func (ab *atomBuilder) BuildAtoms(state store.State, m match.Match) (swap.Atom, 
 	return personalAtom, foreignAtom, nil
 }
 
-func buildAtom(key swap.Key, config config.Config, orderID [32]byte) (swap.Atom, error) {
-	switch key.PriorityCode() {
+func buildAtom(binder binder.Binder, key keystore.Keystore, config network.Config, cc uint32, orderID [32]byte) (swap.Atom, error) {
+	switch cc {
 	case 0:
 		conn, err := btcClient.Connect(config)
 		if err != nil {
 			return nil, err
 		}
-		return btc.NewBitcoinAtom(conn, key, orderID), nil
+		return btc.NewBitcoinAtom(&binder, conn, &key.BitcoinKey, orderID), nil
 	case 1:
 		conn, err := ethClient.Connect(config)
 		if err != nil {
 			return nil, err
 		}
-		return eth.NewEthereumAtom(conn, key, orderID)
+		return eth.NewEthereumAtom(&binder, conn, &key.EthereumKey, orderID)
 	}
 	return nil, fmt.Errorf("Atom Build Failed")
 }
