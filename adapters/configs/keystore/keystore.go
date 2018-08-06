bf3f98736654a986c679a5adf6313609ff9f3fcd
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-08-06 15:33:00 +1000

    Updated keystore interface

--- adapters/configs/keystore/keystore.go
+++ adapters/configs/keystore/keystore.go
@@ -1,46 +1,110 @@
 package keystore
 
 import (
-	"crypto/ecdsa"
 	"encoding/json"
-	"errors"
+	"fmt"
 	"io/ioutil"
 	"sync"
 )
 
-var ErrUnknownChain = errors.New("unknown chain")
+var ErrPrefix = "Config: Keystore: %s"
 
-type Keystore struct {
-	EthereumKey EthereumKey
-	BitcoinKey  BitcoinKey
+type keystore struct {
+	Keys []key `json:"keys"`
 
-	mu   *sync.RWMutex
-	path string
+	keyMap map[uint32][]key
+	mu     *sync.RWMutex
+	path   string
+}
+type Keystore interface {
+	GetKey(uint32, uint32) (Key, error)
+	AppendKey(uint32, uint32, Key)
 }
 
-type Key interface {
-	GetKey() *ecdsa.PrivateKey
-	GetKeyString() (string, error)
-	GetAddress() ([]byte, error)
-	PriorityCode() uint32
+func NewKeystore(priorityCodes []uint32, chains []string, path string) (Keystore, error) {
+	if len(priorityCodes) != len(chains) {
+		return nil, fmt.Errorf(ErrPrefix, "Invalid Parameters")
+	}
+
+	var keystore keystore
+	keystore.mu = new(sync.RWMutex)
+	keystore.keyMap = make(map[uint32][]key)
+	keystore.path = path
+
+	for i := range priorityCodes {
+		key, err := generateKey(priorityCodes[i], chains[i])
+		if err != nil {
+			return nil, err
+		}
+		keystore.Keys = append(keystore.Keys, key)
+	}
+
+	if err := keystore.update(); err != nil {
+		return nil, err
+	}
+
+	for _, key := range keystore.Keys {
+		keystore.keyMap[key.Code] = append(keystore.keyMap[key.Code], key)
+	}
+
+	return &keystore, nil
 }
 
-func LoadKeystore(path string) (Keystore, error) {
-	var keystore Keystore
+func Load(path string) (Keystore, error) {
+	var keystore keystore
 	keystore.path = path
 	keystore.mu = new(sync.RWMutex)
+	keystore.keyMap = make(map[uint32][]key)
 	raw, err := ioutil.ReadFile(path)
 	if err != nil {
-		return keystore, err
+		return &keystore, err
 	}
 	json.Unmarshal(raw, &keystore)
-	return keystore, nil
+	for _, key := range keystore.Keys {
+		keystore.keyMap[key.Code] = append(keystore.keyMap[key.Code], key)
+	}
+	return &keystore, nil
 }
 
-func (keystore *Keystore) Update() error {
+func (keystore *keystore) GetKey(priorityCode, index uint32) (Key, error) {
+	key := keystore.keyMap[priorityCode][index]
+	return NewKey(key)
+}
+
+func (keystore *keystore) AppendKey(priorityCode, index uint32, keyInf Key) {
+	keyStruct := key{
+		keyInf.GetKeyString(),
+		keyInf.PriorityCode(),
+		keyInf.Chain(),
+	}
+	keystore.Keys = append(keystore.Keys, keyStruct)
+	keystore.keyMap[keyStruct.Code] = append(keystore.keyMap[keyStruct.Code], keyStruct)
+}
+
+func generateKey(priorityCode uint32, chain string) (key, error) {
+	switch priorityCode {
+	case 0:
+		btcKey, err := RandomBitcoinKeyString(chain)
+		return key{
+			Code:       0,
+			PrivateKey: btcKey,
+			Network:    chain,
+		}, err
+	case 1:
+		ethKey, err := RandomEthereumKeyString()
+		return key{
+			Code:       1,
+			PrivateKey: ethKey,
+			Network:    chain,
+		}, err
+	}
+	return key{}, fmt.Errorf(ErrPrefix, "Unknown Priority Code")
+}
+
+func (keystore *keystore) update() error {
 	data, err := json.Marshal(keystore)
 	if err != nil {
 		return err
 	}
-	return ioutil.WriteFile(keystore.path, data, 700)
+	return ioutil.WriteFile(keystore.path, data, 0600)
 }
