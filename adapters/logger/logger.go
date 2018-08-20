c8f4fd89e9c6a7818ac7a8d3d420b55d6cb0125b
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-08-20 15:31:26 +1000

    Updated logs

--- adapters/logger/logger.go
+++ adapters/logger/logger.go
@@ -8,6 +8,8 @@ import (
 	"github.com/republicprotocol/renex-swapper-go/services/logger"
 )
 
+const white = "\033[m"
+
 type stdOutLogger struct {
 }
 
@@ -16,13 +18,35 @@ func NewStdOutLogger() logger.Logger {
 }
 
 func (logger *stdOutLogger) LogInfo(orderID [32]byte, msg string) {
-	log.Println(fmt.Sprintf("(%s) %s", order.Fmt(orderID), msg))
+	clr := pickColor(orderID)
+	log.Println(fmt.Sprintf("[INF] (%s%s%s) %s", clr, order.Fmt(orderID), white, msg))
 }
 
 func (logger *stdOutLogger) LogDebug(orderID [32]byte, msg string) {
-	log.Println(fmt.Sprintf("(%s) %s", order.Fmt(orderID), msg))
+	clr := pickColor(orderID)
+	log.Println(fmt.Sprintf("[DEB] (%s%s%s) %s", clr, order.Fmt(orderID), white, msg))
 }
 
 func (logger *stdOutLogger) LogError(orderID [32]byte, msg string) {
-	log.Println(fmt.Sprintf("(%s) %s", order.Fmt(orderID), msg))
+	clr := pickColor(orderID)
+	log.Println(fmt.Sprintf("[ERR] (%s%s%s) %s", clr, order.Fmt(orderID), white, msg))
+}
+
+func pickColor(orderID [32]byte) string {
+	switch int64(orderID[0]) % 7 {
+	case 0:
+		return "\033[30m"
+	case 1:
+		return "\033[31m"
+	case 2:
+		return "\033[32m"
+	case 3:
+		return "\033[33m"
+	case 4:
+		return "\033[34m"
+	case 5:
+		return "\033[35m"
+	default:
+		return "\033[36m"
+	}
 }
