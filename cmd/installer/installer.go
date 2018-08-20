36b70d32304247d966ce261992b89c6eb4554176
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-08-20 15:57:17 +1000

    Updated renguard address

--- cmd/installer/installer.go
+++ cmd/installer/installer.go
@@ -40,7 +40,7 @@ func main() {
 	}
 	cfg.AuthorizedAddresses = addresses
 	cfg.StoreLoc = home + "/.swapper/db"
-	cfg.RenGuardAddr = "renex-watchdog-federation.herokuapp.com"
+	cfg.RenGuardAddr = "renex-watchdog-testnet.herokuapp.com"
 
 	if err := cfg.Update(); err != nil {
 		panic(err)
