b0e17f77983add24aee3fadc00527a082e32a7fe
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-09-17 16:24:26 +1000

    Add logger adapter

--- adapter/logger/logger.go
+++ adapter/logger/logger.go
@@ -1,37 +1,7 @@
 package logger
 
-import (
-	"fmt"
-	"log"
-
-	"github.com/republicprotocol/renex-swapper-go/domain/order"
-	"github.com/republicprotocol/renex-swapper-go/service/logger"
-)
-
-const white = "\033[m"
-
-type stdOutLogger struct {
-}
-
-func NewStdOutLogger() logger.Logger {
-	return &stdOutLogger{}
-}
-
-func (logger *stdOutLogger) LogInfo(orderID [32]byte, msg string) {
-	clr := pickColor(orderID)
-	log.Println(fmt.Sprintf("[INF] (%s%s%s) %s", clr, order.Fmt(orderID), white, msg))
-}
-
-func (logger *stdOutLogger) LogDebug(orderID [32]byte, msg string) {
-	clr := pickColor(orderID)
-	log.Println(fmt.Sprintf("[DEB] (%s%s%s) %s", clr, order.Fmt(orderID), white, msg))
-}
-
-func (logger *stdOutLogger) LogError(orderID [32]byte, msg string) {
-	clr := pickColor(orderID)
-	log.Println(fmt.Sprintf("[ERR] (%s%s%s) %s", clr, order.Fmt(orderID), white, msg))
-}
-
-func pickColor(orderID [32]byte) string {
-	return fmt.Sprintf("\033[3%dm", int64(orderID[0])%7)
+type Logger interface {
+	LogInfo(orderID [32]byte, msg string)
+	LogDebug(orderID [32]byte, msg string)
+	LogError(orderID [32]byte, msg string)
 }
