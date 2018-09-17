71423b62d4b8a1841fd65741fc56978089cb20d1
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-09-17 16:26:25 +1000

    Add watchdog adapter

---
+++ adapter/watchdog/watchdog.go
@@ -0,0 +1,10 @@
+package watchdog
+
+type Watchdog interface {
+	ComplainDelayedAddressSubmission([32]byte) error
+	ComplainDelayedRequestorInitiation([32]byte) error
+	ComplainWrongRequestorInitiation([32]byte) error
+	ComplainDelayedResponderInitiation([32]byte) error
+	ComplainWrongResponderInitiation([32]byte) error
+	ComplainDelayedRequestorRedemption([32]byte) error
+}
