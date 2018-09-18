04cc0ece56691212014ecdd3c61c422652647e33
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-09-19 08:17:48 +1000

    Fixed renex adapter

--- adapter/renex/renex.go
+++ adapter/renex/renex.go
@@ -5,41 +5,52 @@ import (
 	"time"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
 
 	bindings "github.com/republicprotocol/renex-swapper-go/adapter/blockchain/bindings/eth"
 	ethclient "github.com/republicprotocol/renex-swapper-go/adapter/blockchain/clients/eth"
 	"github.com/republicprotocol/renex-swapper-go/adapter/config"
+	"github.com/republicprotocol/renex-swapper-go/adapter/keystore"
+	"github.com/republicprotocol/renex-swapper-go/adapter/network"
 	swapAdapter "github.com/republicprotocol/renex-swapper-go/adapter/swap"
+	"github.com/republicprotocol/renex-swapper-go/adapter/watchdog"
 	"github.com/republicprotocol/renex-swapper-go/domain/match"
 	"github.com/republicprotocol/renex-swapper-go/domain/order"
+	"github.com/republicprotocol/renex-swapper-go/domain/token"
+	"github.com/republicprotocol/renex-swapper-go/service/logger"
 	"github.com/republicprotocol/renex-swapper-go/service/renex"
+	"github.com/republicprotocol/renex-swapper-go/service/state"
 	"github.com/republicprotocol/renex-swapper-go/service/swap"
 )
 
 type renexAdapter struct {
-	swap.Builder
+	state.State
+	keystore.Keystore
+	logger.Logger
+	swap.Swapper
+	network.Network
 	*bindings.RenExSettlement
 }
 
-func New(config config.Config, network swap.Network, watchdog swap.Watchdog, state renex.State, logger swap.Logger, atomBuilder swap.AtomBuilder) (renex.Adapter, error) {
-	conn, err := ethclient.Connect(config)
+func New(config config.Config, keystore keystore.Keystore, network network.Network, watchdog watchdog.Watchdog, state state.State, logger logger.Logger) (renex.Adapter, error) {
+	conn, err := ethclient.Connect(config.Ethereum)
 	if err != nil {
 		return nil, fmt.Errorf("cannot connect to Ethereum blockchain: %v", err)
 	}
 
-	renExSettlement, err := bindings.NewRenExSettlement(conn.RenExSettlement, bind.ContractBackend(conn.Client))
+	renExSettlement, err := bindings.NewRenExSettlement(common.HexToAddress(config.RenEx.Settlement), bind.ContractBackend(conn.Client))
 	if err != nil {
 		return nil, fmt.Errorf("cannot bind to RenEx accounts: %v", err)
 	}
 
 	return &renexAdapter{
+		Keystore:        keystore,
+		State:           state,
+		Logger:          logger,
+		Network:         network,
 		RenExSettlement: renExSettlement,
-		Builder:         swapAdapter.New(network, watchdog, state, logger, atomBuilder),
+		Swapper:         swap.NewSwapper(swapAdapter.New(config, keystore, network, watchdog, state, logger)),
 	}, nil
-}
-
-func (renex *renexAdapter) NewSwapAdapter(req swap.Request) (swap.Adapter, error) {
-	return renex.Builder.New(req)
 }
 
 // GetOrderMatch checks if a match is found and returns the match object. It
@@ -52,13 +63,24 @@ func (renex *renexAdapter) GetOrderMatch(orderID order.ID, waitTill int64) (matc
 		}
 
 		if ReceiveCurrency != SendCurrency {
-			return match.NewMatch(PersonalOrder, ForeignOrder, SendValue, ReceiveValue, SendCurrency, ReceiveCurrency), nil
+			return match.NewMatch(PersonalOrder, ForeignOrder, SendValue, ReceiveValue, token.Token(SendCurrency), token.Token(ReceiveCurrency)), nil
 		}
 
 		if time.Now().Unix() > waitTill {
 			return nil, fmt.Errorf("Timed out")
 		}
-
 		time.Sleep(15 * time.Second)
+	}
+}
+
+// GetAddress corresponding to the given token.
+func (renex *renexAdapter) GetAddress(tok token.Token) []byte {
+	switch tok {
+	case token.BTC:
+		return []byte(renex.Keystore.GetKey(tok).(keystore.BitcoinKey).AddressString)
+	case token.ETH:
+		return []byte(renex.Keystore.GetKey(tok).(keystore.EthereumKey).Address.String())
+	default:
+		panic("Unexpected token")
 	}
 }
