ef064913e2b908b08555cf060904a090017f8eaa
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-09-18 14:14:25 +1000

    Fixed typos in network adapter

--- adapter/network/network.go
+++ adapter/network/network.go
@@ -5,6 +5,6 @@ import "github.com/republicprotocol/renex-swapper-go/domain/order"
 type Network interface {
 	SendOwnerAddress(order.ID, []byte) error
 	SendSwapDetails(order.ID, []byte) error
-	RecieveOwnerAddress(order.ID, int64) ([]byte, error)
-	RecieveSwapDetails(order.ID, int64) ([]byte, error)
+	ReceiveOwnerAddress(order.ID, int64) ([]byte, error)
+	ReceiveSwapDetails(order.ID, int64) ([]byte, error)
 }
