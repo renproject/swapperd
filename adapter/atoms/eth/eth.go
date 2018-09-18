25e37022337ee01a1c50e5e04feb27b36ac127dd
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-09-18 14:13:01 +1000

    Updated ethereum atom

--- adapter/atoms/eth/eth.go
+++ adapter/atoms/eth/eth.go
@@ -13,14 +13,10 @@ import (
 	ethclient "github.com/republicprotocol/renex-swapper-go/adapter/blockchain/clients/eth"
 	"github.com/republicprotocol/renex-swapper-go/adapter/config"
 	"github.com/republicprotocol/renex-swapper-go/adapter/keystore"
-	"github.com/republicprotocol/renex-swapper-go/domain/order"
+	"github.com/republicprotocol/renex-swapper-go/adapter/network"
 	"github.com/republicprotocol/renex-swapper-go/domain/token"
 	"github.com/republicprotocol/renex-swapper-go/service/swap"
 )
-
-type Adapter interface {
-	RecieveSwapDetails(order.ID, int64) ([]byte, error)
-}
 
 // EthereumData
 type EthereumData struct {
@@ -34,12 +30,12 @@ type EthereumAtom struct {
 	client  ethclient.Conn
 	key     keystore.EthereumKey
 	binding *bindings.AtomicSwap
-	adapter Adapter
+	network network.Network
 	data    EthereumData
 }
 
 // NewEthereumAtom returns a new Ethereum RequestAtom instance
-func NewEthereumAtom(adapter Adapter, conf config.EthereumNetwork, key keystore.EthereumKey, orderID [32]byte) (swap.Atom, error) {
+func NewEthereumAtom(network network.Network, conf config.EthereumNetwork, key keystore.EthereumKey, orderID [32]byte) (swap.Atom, error) {
 	conn, err := ethclient.Connect(conf)
 	if err != nil {
 		return &EthereumAtom{}, err
@@ -59,7 +55,7 @@ func NewEthereumAtom(adapter Adapter, conf config.EthereumNetwork, key keystore.
 		key:     key,
 		binding: contract,
 		orderID: orderID,
-		adapter: adapter,
+		network: network,
 		data: EthereumData{
 			SwapID: swapID,
 		},
@@ -73,14 +69,12 @@ func (atom *EthereumAtom) Initiate(to []byte, hash [32]byte, value *big.Int, exp
 	atom.key.TransactOpts.Value = value
 	atom.key.TransactOpts.GasLimit = 3000000
 	atom.data.HashLock = hash
-
-	tx, err := atom.binding.Initiate(atom.key.TransactOpts, atom.data.SwapID, common.BytesToAddress(to), hash, big.NewInt(expiry))
+	tx, err := atom.binding.Initiate(atom.key.TransactOpts, atom.data.SwapID, common.HexToAddress(string(to)), hash, big.NewInt(expiry))
 	atom.key.TransactOpts.Value = prevValue
 	atom.key.TransactOpts.GasLimit = prevGasLimit
 	if err != nil {
 		return err
 	}
-
 	_, err = atom.client.PatchedWaitMined(atom.context, tx)
 	return err
 }
@@ -124,7 +118,7 @@ func (atom *EthereumAtom) Refund() error {
 
 // Audit an Atom swap by calling a function on ethereum
 func (atom *EthereumAtom) Audit() ([32]byte, []byte, *big.Int, int64, error) {
-	details, err := atom.adapter.RecieveSwapDetails(atom.orderID, time.Now().Add(15*time.Minute).Unix())
+	details, err := atom.network.ReceiveSwapDetails(atom.orderID, time.Now().Add(15*time.Minute).Unix())
 	if err != nil {
 		return [32]byte{}, nil, nil, 0, err
 	}
@@ -136,12 +130,12 @@ func (atom *EthereumAtom) Audit() ([32]byte, []byte, *big.Int, int64, error) {
 	if err != nil {
 		return [32]byte{}, nil, nil, 0, err
 	}
-	return auditReport.SecretLock, auditReport.To.Bytes(), auditReport.Value, auditReport.Timelock.Int64(), nil
+	return auditReport.SecretLock, []byte(auditReport.To.String()), auditReport.Value, auditReport.Timelock.Int64(), nil
 }
 
 // AuditSecret audits the secret of an Atom swap by calling a function on ethereum
 func (atom *EthereumAtom) AuditSecret() ([32]byte, error) {
-	details, err := atom.adapter.RecieveSwapDetails(atom.orderID, time.Now().Add(15*time.Minute).Unix())
+	details, err := atom.network.ReceiveSwapDetails(atom.orderID, time.Now().Add(15*time.Minute).Unix())
 	if err != nil {
 		return [32]byte{}, err
 	}
@@ -154,7 +148,7 @@ func (atom *EthereumAtom) AuditSecret() ([32]byte, error) {
 
 // RedeemedAt returns the timestamp at which the atom is redeemed
 func (atom *EthereumAtom) RedeemedAt() (int64, error) {
-	details, err := atom.adapter.RecieveSwapDetails(atom.orderID, time.Now().Add(15*time.Minute).Unix())
+	details, err := atom.network.ReceiveSwapDetails(atom.orderID, time.Now().Add(15*time.Minute).Unix())
 	if err != nil {
 		return 0, err
 	}
