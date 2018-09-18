9c1d8e46c15695754a8bd88a562d716784be8a17
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-09-19 08:22:32 +1000

    Add mock network driver

---
+++ driver/network/mock.go
@@ -0,0 +1,71 @@
+package network
+
+import (
+	"bytes"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/republicprotocol/renex-swapper-go/adapter/network"
+	"github.com/republicprotocol/renex-swapper-go/domain/order"
+)
+
+type mock struct {
+	traderAddresses   map[order.ID][]byte
+	traderSwapDetails map[order.ID][]byte
+	mu                *sync.RWMutex
+}
+
+func NewMock() network.Network {
+	return &mock{
+		traderAddresses:   map[order.ID][]byte{},
+		traderSwapDetails: map[order.ID][]byte{},
+		mu:                new(sync.RWMutex),
+	}
+}
+
+func (mock *mock) SendOwnerAddress(orderID order.ID, address []byte) error {
+	mock.mu.Lock()
+	defer mock.mu.Unlock()
+	mock.traderAddresses[orderID] = address
+	return nil
+}
+
+func (mock *mock) SendSwapDetails(orderID order.ID, swapDetails []byte) error {
+	mock.mu.Lock()
+	defer mock.mu.Unlock()
+	mock.traderSwapDetails[orderID] = swapDetails
+	return nil
+}
+
+func (mock *mock) ReceiveOwnerAddress(orderID order.ID, waitTill int64) ([]byte, error) {
+	for {
+		mock.mu.Lock()
+		details := mock.traderAddresses[orderID]
+		mock.mu.Unlock()
+		if bytes.Compare(details, []byte{}) != 0 {
+			return details, nil
+		}
+		if time.Now().Unix() > waitTill {
+			break
+		}
+		time.Sleep(10 * time.Second)
+	}
+	return []byte{}, fmt.Errorf("Timed Out")
+}
+
+func (mock *mock) ReceiveSwapDetails(orderID order.ID, waitTill int64) ([]byte, error) {
+	for {
+		mock.mu.Lock()
+		details := mock.traderSwapDetails[orderID]
+		mock.mu.Unlock()
+		if bytes.Compare(details, []byte{}) != 0 {
+			return details, nil
+		}
+		if time.Now().Unix() > waitTill {
+			break
+		}
+		time.Sleep(10 * time.Second)
+	}
+	return []byte{}, fmt.Errorf("Timed Out")
+}
