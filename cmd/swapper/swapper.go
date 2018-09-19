08ad2b0c232c98b02991cca53a555f2881cf3663
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-09-19 15:21:38 +1000

    Update the RenEx cmd tool

--- cmd/swapper/swapper.go
+++ cmd/swapper/swapper.go
@@ -8,9 +8,8 @@ import (
 	"os"
 	"os/signal"
 
-	"github.com/republicprotocol/renex-swapper-go/adapter/config"
+	guardianAdapter "github.com/republicprotocol/renex-swapper-go/adapter/guardian"
 	"github.com/republicprotocol/renex-swapper-go/adapter/http"
-	"github.com/republicprotocol/renex-swapper-go/adapter/keystore"
 	renexAdapter "github.com/republicprotocol/renex-swapper-go/adapter/renex"
 	stateAdapter "github.com/republicprotocol/renex-swapper-go/adapter/state"
 	configDriver "github.com/republicprotocol/renex-swapper-go/driver/config"
@@ -27,12 +26,13 @@ import (
 
 func main() {
 	port := flag.String("port", "18516", "HTTP Atom port")
-	repNet := flag.String("network", "nightly", "Republic Protocol Network")
+	repNet := flag.String("network", "testnet", "Republic Protocol Network")
 	keyphrase := flag.String("keyphrase", "", "Keyphrase to unlock keystore")
+	location := flag.String("loc", getHome()+"/.swapper", "Location of the swapper directory")
 	flag.Parse()
 
-	conf := configDriver.New(*repNet)
-	ks := keystoreDriver.LoadFromFile(*repNet, *keyphrase)
+	conf := configDriver.New(*repNet, *location)
+	ks := keystoreDriver.LoadFromFile(*repNet, *location, *keyphrase)
 	db, err := storeDriver.NewLevelDB(conf.StoreLocation)
 	if err != nil {
 		panic(err)
@@ -41,16 +41,14 @@ func main() {
 	state := state.NewState(stateAdapter.New(db, logger))
 	ingressNet := network.NewIngress(conf.RenEx.Ingress)
 	nopWatchdog := watchdogDriver.NewMock()
-	swapperAdapter, err := renexAdapter.New(conf, ks, ingressNet, nopWatchdog, state, logger)
-	if err != nil {
-		panic(err)
-	}
-	renexSwapper := renex.NewRenEx(swapperAdapter)
 
-	guardian, err := buildGuardian(conf, ks, state)
+	binder, err := renexAdapter.NewBinder(conf)
 	if err != nil {
 		panic(err)
 	}
+
+	renexSwapper := renex.NewRenEx(renexAdapter.New(conf, ks, ingressNet, nopWatchdog, state, logger, binder))
+	guardian := guardian.NewGuardian(guardianAdapter.New(conf, ks, ingressNet, state, logger))
 
 	errCh1 := renexSwapper.Start()
 	renexSwapper.Notify()
@@ -85,11 +83,6 @@ func main() {
 	httpAdapter := http.NewAdapter(conf, ks, renexSwapper)
 	log.Println(fmt.Sprintf("0.0.0.0:%s", *port))
 	log.Fatal(netHttp.ListenAndServe(fmt.Sprintf(":%s", *port), httpDriver.NewServer(httpAdapter)))
-
-}
-
-func buildGuardian(conf config.Config, keystore keystore.Keystore, state state.State) (guardian.Guardian, error) {
-	return guardian.NewGuardian(), nil
 }
 
 func getHome() string {
