6ac05311792c5f6ed5b8c70869d6fcb8b69beef6
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-09-19 15:18:49 +1000

    Updated config driver

--- driver/config/config.go
+++ driver/config/config.go
@@ -4,14 +4,12 @@ import (
 	"fmt"
 
 	"github.com/republicprotocol/renex-swapper-go/adapter/config"
-	"github.com/republicprotocol/renex-swapper-go/utils"
 )
 
 // Global is the default global config
 var Global = config.Config{
 	Version:             "0.1.0",
 	SupportedCurrencies: []string{"ETH", "BTC"},
-	StoreLocation:       utils.GetHome() + "/.swapper/db",
 	Ethereum:            EthereumKovan,
 	Bitcoin:             BitcoinMainnet,
 }
@@ -39,24 +37,28 @@ func NewRenExNetwork(net string) config.RenExNetwork {
 	return config.RenExNetwork{
 		Network:  net,
 		Watchdog: fmt.Sprintf("renex-watchdog-%s.herokuapp.com", net),
-		Ingress:  fmt.Sprintf("renex-ingress-%s.herokuapp.com", net),
+		Ingress:  fmt.Sprintf("renex-ingress-nightly.herokuapp.com"),
 	}
 }
 
 // New creates a new config object from the config data object
-func New(net string) config.Config {
+func New(net, loc string) config.Config {
 	conf := Global
+	conf.StoreLocation = loc + "/db"
 	conf.RenEx = NewRenExNetwork(net)
 	switch net {
 	case "nightly":
+		conf.RenEx.Orderbook = ""
 		conf.RenEx.Settlement = ""
 		conf.Ethereum.Swapper = "0xa80c64Cc2c3e29B44CaB2475F6eAd0D523715A4E"
 		return conf
 	case "falcon":
+		conf.RenEx.Orderbook = ""
 		conf.RenEx.Settlement = ""
 		conf.Ethereum.Swapper = ""
 		return conf
 	case "testnet":
+		conf.RenEx.Orderbook = ""
 		conf.RenEx.Settlement = ""
 		conf.Ethereum.Swapper = ""
 		return conf
