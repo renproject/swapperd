992c26708507cc0e85133139921b6dded20fd1bb
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-09-23 14:39:57 +1000

    Updated mock RenEx adapter

--- adapter/renex/mock.go
+++ adapter/renex/mock.go
@@ -1,18 +1,18 @@
 package renex
 
 import (
-	"github.com/republicprotocol/renex-swapper-go/domain/match"
 	"github.com/republicprotocol/renex-swapper-go/domain/order"
+	"github.com/republicprotocol/renex-swapper-go/domain/swap"
 )
 
 type mockBinder struct {
-	orderbook map[order.ID]match.Match
+	orderbook map[order.ID]swap.Match
 }
 
-func NewMockBinder(matches ...match.Match) (Binder, error) {
-	orderbook := map[order.ID]match.Match{}
+func NewMockBinder(matches ...swap.Match) (Binder, error) {
+	orderbook := map[order.ID]swap.Match{}
 	for _, match := range matches {
-		orderbook[match.PersonalOrderID()] = match
+		orderbook[match.PersonalOrderID] = match
 	}
 	return &mockBinder{
 		orderbook: orderbook,
@@ -20,12 +20,12 @@ func NewMockBinder(matches ...match.Match) (Binder, error) {
 }
 
 // GetOrderMatch gets the order match from the mock renex adapter.
-func (binder *mockBinder) GetOrderMatch(orderID order.ID, waitTill int64) (match.Match, error) {
+func (binder *mockBinder) GetOrderMatch(orderID order.ID, waitTill int64) (swap.Match, error) {
 	return binder.orderbook[orderID], nil
 }
 
 // SubmitOrderMatch submits an order match to the mock binder adapter.
-func (binder *mockBinder) SubmitOrderMatch(match match.Match) error {
-	binder.orderbook[match.PersonalOrderID()] = match
+func (binder *mockBinder) SubmitOrderMatch(match swap.Match) error {
+	binder.orderbook[match.PersonalOrderID] = match
 	return nil
 }
