9c2e945c8f5af9100fcd057416d22278d7cdd3e3
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-09-23 14:46:58 +1000

    Updated RenEx service

--- service/renex/adapter.go
+++ service/renex/adapter.go
@@ -1,19 +1,30 @@
 package renex
 
 import (
-	"github.com/republicprotocol/renex-swapper-go/domain/match"
-	"github.com/republicprotocol/renex-swapper-go/domain/order"
+	"github.com/republicprotocol/renex-swapper-go/domain/swap"
 	"github.com/republicprotocol/renex-swapper-go/domain/token"
 	"github.com/republicprotocol/renex-swapper-go/service/logger"
 	"github.com/republicprotocol/renex-swapper-go/service/state"
-	"github.com/republicprotocol/renex-swapper-go/service/swap"
+	swapService "github.com/republicprotocol/renex-swapper-go/service/swap"
 )
 
 type Adapter interface {
 	state.State
 	logger.Logger
-	swap.Swapper
-	swap.Network
-	GetOrderMatch(orderID order.ID, waitTill int64) (match.Match, error)
-	GetAddress(token.Token) []byte
+	swapService.Swapper
+	Network
+	GetOrderMatch(orderID [32]byte, waitTill int64) (swap.Match, error)
+	GetAddresses(token.Token, token.Token) (string, string)
+}
+
+type Network interface {
+	SendSwapDetails([32]byte, SwapDetails) error
+	ReceiveSwapDetails([32]byte, int64) (SwapDetails, error)
+}
+
+type SwapDetails struct {
+	SecretHash         [32]byte `json:"secretHash"`
+	TimeLock           int64    `json:"timeLock"`
+	SendToAddress      string   `json:"sendToAddress"`
+	ReceiveFromAddress string   `json:"receiveFromAddress"`
 }
