887bc18a7fc506a7fe44083fd0990bf93e1464c8
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-09-23 14:46:09 +1000

    Updated stdout logger

--- driver/logger/stdout.go
+++ driver/logger/stdout.go
@@ -1,10 +1,10 @@
 package logger
 
 import (
+	"encoding/base64"
 	"fmt"
 	"log"
 
-	"github.com/republicprotocol/renex-swapper-go/domain/order"
 	"github.com/republicprotocol/renex-swapper-go/service/logger"
 )
 
@@ -19,17 +19,17 @@ func NewStdOut() logger.Logger {
 
 func (logger *stdOut) LogInfo(orderID [32]byte, msg string) {
 	clr := pickColor(orderID)
-	log.Println(fmt.Sprintf("[INF] (%s%s%s) %s", clr, order.Fmt(orderID), white, msg))
+	log.Println(fmt.Sprintf("[INF] (%s%s%s) %s", clr, base64.StdEncoding.EncodeToString(orderID[:]), white, msg))
 }
 
 func (logger *stdOut) LogDebug(orderID [32]byte, msg string) {
 	clr := pickColor(orderID)
-	log.Println(fmt.Sprintf("[DEB] (%s%s%s) %s", clr, order.Fmt(orderID), white, msg))
+	log.Println(fmt.Sprintf("[DEB] (%s%s%s) %s", clr, base64.StdEncoding.EncodeToString(orderID[:]), white, msg))
 }
 
 func (logger *stdOut) LogError(orderID [32]byte, msg string) {
 	clr := pickColor(orderID)
-	log.Println(fmt.Sprintf("[ERR] (%s%s%s) %s", clr, order.Fmt(orderID), white, msg))
+	log.Println(fmt.Sprintf("[ERR] (%s%s%s) %s", clr, base64.StdEncoding.EncodeToString(orderID[:]), white, msg))
 }
 
 func pickColor(orderID [32]byte) string {
