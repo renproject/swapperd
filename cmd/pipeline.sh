d68ea5ae74c793c0917c329b27ec59c6fc739e39
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-10-09 09:14:41 +1100

    Updated pipeline script

--- cmd/pipeline.sh
+++ cmd/pipeline.sh
@@ -3,10 +3,12 @@
 mkdir -p bin
 cd installer
 go build .
-xgo --targets=linux/amd64 .
+xgo --targets=linux/amd64,windows/amd64 .
 cd ../swapper/
 go build .
-xgo --targets=linux/amd64 .
+xgo --targets=linux/amd64,windows/amd64 .
+cd ../installer-win/
+xgo --targets=windows/amd64 .
 
 cd ..
 mv installer/installer bin/installer
@@ -16,5 +18,13 @@ zip -r swapper_darwin_amd64.zip bin
 mv installer/installer-linux-amd64 bin/installer
 mv swapper/swapper-linux-amd64 bin/swapper
 zip -r swapper_linux_amd64.zip bin
+
+rm -rf bin
+mkdir -p bin
+
+mv installer/installer-windows-4.0-amd64.exe bin/installer.exe
+mv swapper/swapper-windows-4.0-amd64.exe bin/swapper.exe
+mv installer-win/installer-win-windows-4.0-amd64.exe bin/service.exe
+zip -r swapper_windows_amd64.zip bin
 
 rm -rf bin
