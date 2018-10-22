f4d6ea32f24fabe24b20a51a3fd96152449f615d
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-10-22 14:45:23 +1100

    adapter/storage: Add storage adapter

--- adapter/storage/storage.go
+++ adapter/storage/storage.go
@@ -1,34 +1,107 @@
 package storage
 
 import (
+	"encoding/json"
+	"sync"
+
+	"github.com/republicprotocol/swapperd/core/swapper"
 	"github.com/republicprotocol/swapperd/foundation"
 )
 
-type Storage interface {
-	AddSwap(swap foundation.Swap) error
-	DeleteSwap(swapID foundation.SwapID) error
-	LoadSwaps() []foundation.Swap
-}
-
-func NewStorage() Storage {
-	return &storage{}
+type Store interface {
+	Read([]byte) ([]byte, error)
+	Write([]byte, []byte) error
+	Delete([]byte) error
 }
 
 type storage struct {
+	mu    *sync.RWMutex
+	store Store
+}
+
+type SwapStorage struct {
+	Swaps        []foundation.Swap `json:"swaps"`
+	PendingSwaps []foundation.Swap `json:"pendingSwaps"`
+}
+
+func New(store Store) swapper.Storage {
+	return &storage{
+		mu:    new(sync.RWMutex),
+		store: store,
+	}
 }
 
 func (storage *storage) AddSwap(swap foundation.Swap) error {
-	return nil
+	swapStorageBytes, err := storage.store.Read([]byte("SwapStorage"))
+	if err != nil {
+		return err
+	}
+
+	swapStorage := SwapStorage{}
+	if err := json.Unmarshal(swapStorageBytes, &swapStorage); err != nil {
+		return err
+	}
+
+	swapStorage.Swaps = append(swapStorage.Swaps, swap)
+	swapStorage.PendingSwaps = append(swapStorage.Swaps, swap)
+
+	swapStorageBytes, err = json.Marshal(swapStorage)
+	if err != nil {
+		return err
+	}
+
+	return storage.store.Write([]byte("SwapStorage"), swapStorageBytes)
 }
 
-func (storage *storage) DeleteSwap(orderID foundation.SwapID) error {
-	return nil
+func (storage *storage) DeleteSwap(swapID foundation.SwapID) error {
+	swapStorageBytes, err := storage.store.Read([]byte("SwapStorage"))
+	if err != nil {
+		return err
+	}
+
+	swapStorage := SwapStorage{}
+	if err := json.Unmarshal(swapStorageBytes, &swapStorage); err != nil {
+		return err
+	}
+
+	for i, swap := range swapStorage.PendingSwaps {
+		if swap.ID == swapID {
+			swapStorage.PendingSwaps = append(swapStorage.PendingSwaps[:i], swapStorage.PendingSwaps[:i+1]...)
+		}
+	}
+
+	swapStorageBytes, err = json.Marshal(swapStorage)
+	if err != nil {
+		return err
+	}
+
+	return storage.store.Write([]byte("SwapStorage"), swapStorageBytes)
+}
+
+func (storage *storage) LoadPendingSwaps() []foundation.Swap {
+	swapStorageBytes, err := storage.store.Read([]byte("SwapStorage"))
+	if err != nil {
+		return []foundation.Swap{}
+	}
+
+	swapStorage := SwapStorage{}
+	if err := json.Unmarshal(swapStorageBytes, &swapStorage); err != nil {
+		return []foundation.Swap{}
+	}
+
+	return swapStorage.PendingSwaps
 }
 
 func (storage *storage) LoadSwaps() []foundation.Swap {
-	return []foundation.Swap{}
-}
+	swapStorageBytes, err := storage.store.Read([]byte("SwapStorage"))
+	if err != nil {
+		return []foundation.Swap{}
+	}
 
-func (storage *storage) GetSwaps() []foundation.Swap {
-	return []foundation.Swap{}
+	swapStorage := SwapStorage{}
+	if err := json.Unmarshal(swapStorageBytes, &swapStorage); err != nil {
+		return []foundation.Swap{}
+	}
+
+	return swapStorage.Swaps
 }
