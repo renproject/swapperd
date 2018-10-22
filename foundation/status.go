a823579c8d98999ddce398daf7e4e14faade91a5
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-10-22 14:47:40 +1100

    Add swap statuses to the foundation

---
+++ foundation/status.go
@@ -0,0 +1,10 @@
+package foundation
+
+type Status int64
+
+var PENDING = Status(0)
+var INITIATED = Status(1)
+var AUDITED = Status(2)
+var AUDIT_FAILED = Status(3)
+var REDEEMED = Status(4)
+var REFUNDED = Status(5)
