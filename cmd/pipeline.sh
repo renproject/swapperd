7cae2f29c1d0596f7833b4a083d18aa59bb11c5a
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-10-24 17:04:38 +1100

    cmd: updated installation scripts

--- cmd/pipeline.sh
+++ cmd/pipeline.sh
@@ -3,28 +3,17 @@
 mkdir -p bin
 cd installer
 go build .
-xgo --targets=linux/amd64,windows/amd64 .
-cd ../swapper/
+xgo --targets=linux/amd64 .
+cd ../swapperd/
 go build .
-xgo --targets=linux/amd64,windows/amd64 .
-cd ../installer-win/
-xgo --targets=windows/amd64 .
+xgo --targets=linux/amd64 .
 
 cd ..
 mv installer/installer bin/installer
-mv swapper/swapper bin/swapper
+mv swapperd/swapperd bin/swapperd
 zip -r swapper_darwin_amd64.zip bin
 
 mv installer/installer-linux-amd64 bin/installer
-mv swapper/swapper-linux-amd64 bin/swapper
+mv swapperd/swapperd-linux-amd64 bin/swapperd
 zip -r swapper_linux_amd64.zip bin
-
-rm -rf bin
-mkdir -p bin
-
-mv installer/installer-windows-4.0-amd64.exe bin/installer.exe
-mv swapper/swapper-windows-4.0-amd64.exe bin/swapper.exe
-mv installer-win/installer-win-windows-4.0-amd64.exe bin/service.exe
-zip -r swapper_windows_amd64.zip bin
-
 rm -rf bin
