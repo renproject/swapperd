b1d0c1130ad46cfa9f06c857daeb5f1c82519b98
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-10-24 16:58:51 +1100

    adapter/balance: implemented balance book

--- adapter/balance/balance.go
+++ adapter/balance/balance.go
@@ -4,81 +4,160 @@ import (
 	"context"
 	"fmt"
 	"math/big"
+	"time"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 
-	"github.com/republicprotocol/beth-go"
 	"github.com/republicprotocol/swapperd/adapter/account"
-	"github.com/republicprotocol/swapperd/adapter/eth/erc20"
+	"github.com/republicprotocol/swapperd/adapter/binder/erc20"
 	"github.com/republicprotocol/swapperd/foundation"
 )
 
-type Request struct {
-	token    foundation.Token
-	response chan<- *big.Int
+type Query struct {
+	password string
+	response chan<- map[foundation.Token]Balance
 	err      chan<- error
 }
 
-func GetBalance(accounts account.Accounts, requestCh <-chan Request) {
+type Balance struct {
+	Address string
+	Amount  *big.Int
+}
+
+func NewQuery(password string) (Query, <-chan map[foundation.Token]Balance, <-chan error) {
+	response := make(chan map[foundation.Token]Balance, 1)
+	err := make(chan error, 1)
+	return Query{
+		password: password,
+		response: response,
+		err:      err,
+	}, response, err
+}
+
+type Book interface {
+	Run(done <-chan struct{}, queries <-chan Query)
+}
+
+type book struct {
+	accounts        account.Accounts
+	supportedTokens []foundation.Token
+}
+
+func New(accounts account.Accounts) Book {
+	return &book{
+		accounts: accounts,
+		supportedTokens: []foundation.Token{
+			foundation.TokenBTC,
+			foundation.TokenETH,
+			foundation.TokenWBTC,
+		},
+	}
+}
+
+func (book *book) Run(done <-chan struct{}, queries <-chan Query) {
 	for {
 		select {
-		case req, ok := <-requestCh:
+		case <-done:
+			return
+		case query, ok := <-queries:
 			if !ok {
 				return
 			}
-			switch req.token {
-			case foundation.TokenBTC:
-				account := accounts.GetBitcoinAccount()
-				address, err := account.Address()
-				if err != nil {
-					req.err <- err
-					continue
-				}
-				balance, err := account.Balance(context.Background(), address.String(), 0)
-				if err != nil {
-					req.err <- err
-					continue
-				}
-				req.response <- big.NewInt(balance)
-			case foundation.TokenETH:
-				account := accounts.GetEthereumAccount()
-				client := account.EthClient()
-				bal, err := client.BalanceOf(context.Background(), account.Address())
-				if err != nil {
-					req.err <- err
-					continue
-				}
-				req.response <- bal
-			case foundation.TokenWBTC:
-				bal, err := erc20Balance(accounts.GetEthereumAccount(), foundation.TokenWBTC)
-				if err != nil {
-					req.err <- err
-					continue
-				}
-				req.response <- bal
-			default:
-				req.err <- foundation.NewErrUnsupportedToken(req.token.Name)
-			}
+			balanceMap, err := book.balances(query.password)
+			query.err <- err
+			query.response <- balanceMap
 		}
 	}
 }
 
-func erc20Balance(account beth.Account, token foundation.Token) (balance *big.Int, err error) {
+func (book *book) balances(password string) (map[foundation.Token]Balance, error) {
+	balanceMap := map[foundation.Token]Balance{}
+	for _, token := range book.supportedTokens {
+		balance, err := book.getBalance(token, password)
+		if err != nil {
+			return balanceMap, err
+		}
+		balanceMap[token] = balance
+	}
+	return balanceMap, nil
+}
+
+func (book *book) getBalance(token foundation.Token, password string) (Balance, error) {
+	switch token {
+	case foundation.TokenBTC:
+		return book.btcBalance(password)
+	case foundation.TokenETH:
+		return book.ethBalance(password)
+	case foundation.TokenWBTC:
+		return book.erc20Balance(password, token)
+	}
+	return Balance{}, foundation.NewErrUnsupportedToken(token.Name)
+}
+
+func (book *book) btcBalance(password string) (Balance, error) {
+	account, err := book.accounts.GetBitcoinAccount(password)
+	if err != nil {
+		return Balance{}, err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	address, err := account.Address()
+	if err != nil {
+		return Balance{}, err
+	}
+	balance, err := account.Balance(ctx, address.EncodeAddress(), 0)
+	if err != nil {
+		return Balance{}, err
+	}
+	return Balance{
+		Address: address.EncodeAddress(),
+		Amount:  big.NewInt(balance),
+	}, nil
+}
+
+func (book *book) ethBalance(password string) (Balance, error) {
+	account, err := book.accounts.GetEthereumAccount(password)
+	if err != nil {
+		return Balance{}, err
+	}
 	client := account.EthClient()
+	balance, err := client.BalanceOf(context.Background(), account.Address())
+	if err != nil {
+		return Balance{}, err
+	}
+	return Balance{
+		Address: account.Address().String(),
+		Amount:  balance,
+	}, nil
+}
+
+func (book *book) erc20Balance(password string, token foundation.Token) (Balance, error) {
+	account, err := book.accounts.GetEthereumAccount(password)
+	if err != nil {
+		return Balance{}, err
+	}
+	client := account.EthClient()
+
 	tokenAddr, err := account.ReadAddress(fmt.Sprintf("ERC20:%s", token.Name))
 	if err != nil {
-		return
+		return Balance{}, err
 	}
 	erc20Contract, err := erc20.NewCompatibleERC20(tokenAddr, bind.ContractBackend(client.EthClient()))
 	if err != nil {
-		return
+		return Balance{}, err
 	}
-	err = client.Get(
+	var balance *big.Int
+	if err := client.Get(
 		context.Background(),
 		func() error {
 			balance, err = erc20Contract.BalanceOf(&bind.CallOpts{}, account.Address())
 			return err
 		},
-	)
-	return
+	); err != nil {
+		return Balance{}, err
+	}
+	return Balance{
+		Address: account.Address().String(),
+		Amount:  balance,
+	}, nil
 }
