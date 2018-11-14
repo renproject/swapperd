e5a8c7cfe5e9e042fc223328dee93d1b412cc379
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-11-14 16:24:18 +1100

    adapter/client: add http client adapter

---
+++ adapter/client/client.go
@@ -0,0 +1,96 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/big"
+	"net/http"
+	"time"
+
+	"github.com/republicprotocol/swapperd/core/swapper"
+
+	"github.com/republicprotocol/swapperd/foundation"
+)
+
+type client struct {
+}
+
+func New() swapper.SwapFiller {
+	return &client{}
+}
+
+func (client *client) DelayCallback(partialSwap foundation.SwapBlob) (foundation.SwapBlob, error) {
+	for {
+		data, err := json.MarshalIndent(partialSwap, "", "  ")
+		if err != nil {
+			return partialSwap, err
+		}
+		buf := bytes.NewBuffer(data)
+
+		resp, err := http.Post(fmt.Sprintf(partialSwap.DelayCallbackURL), "application/json", buf)
+		if err != nil {
+			return partialSwap, err
+		}
+
+		respBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return partialSwap, err
+		}
+
+		filledSwap := foundation.SwapBlob{}
+		if resp.StatusCode == 200 {
+			if err := json.Unmarshal(respBytes, &filledSwap); err != nil {
+				return partialSwap, err
+			}
+			return verifyDelaySwap(partialSwap, filledSwap)
+		}
+
+		if resp.StatusCode == 404 {
+			time.Sleep(30 * time.Second)
+			continue
+		}
+
+		return partialSwap, fmt.Errorf("unexpected error %d: %s", resp.StatusCode, respBytes)
+	}
+}
+
+func verifyDelaySwap(partialSwap, filledSwap foundation.SwapBlob) (foundation.SwapBlob, error) {
+	initialMinReceiveValue, ok := big.NewInt(0).SetString(partialSwap.MinimumReceiveAmount, 10)
+	if !ok {
+		return partialSwap, fmt.Errorf("corrupted minimum receive value")
+	}
+
+	initialMaxSendValue, ok := big.NewInt(0).SetString(partialSwap.SendAmount, 10)
+	if !ok {
+		return partialSwap, fmt.Errorf("corrupted send value")
+	}
+
+	initialReceiveValue, ok := big.NewInt(0).SetString(partialSwap.ReceiveAmount, 10)
+	if !ok {
+		return partialSwap, fmt.Errorf("corrupted receive value")
+	}
+
+	filledReceiveValue, ok := big.NewInt(0).SetString(filledSwap.ReceiveAmount, 10)
+	if !ok {
+		return partialSwap, fmt.Errorf("corrupted filled receive value")
+	}
+
+	filledSendValue, ok := big.NewInt(0).SetString(filledSwap.SendAmount, 10)
+	if !ok {
+		return partialSwap, fmt.Errorf("corrupted filled send value")
+	}
+
+	if initialMinReceiveValue.Cmp(filledReceiveValue) > 0 || initialMaxSendValue.Cmp(filledSendValue) < 0 {
+		return partialSwap, fmt.Errorf("invalid filled swap receive value too low or send value too high")
+	}
+
+	if filledReceiveValue.Mul(filledReceiveValue, initialMaxSendValue).Cmp(initialReceiveValue.Mul(initialReceiveValue, filledSendValue)) < 0 {
+		return partialSwap, fmt.Errorf("invalid filled swap unfavorable price")
+	}
+
+	filledSwap.Delay = false
+
+	return filledSwap, nil
+}
---
+++ adapter/client/client_test.go
@@ -0,0 +1,269 @@
+package client_test
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/republicprotocol/swapperd/adapter/client"
+	"github.com/republicprotocol/swapperd/foundation"
+	"github.com/rs/cors"
+)
+
+var _ = Describe("Server Adapter", func() {
+	writeError := func(w http.ResponseWriter, statusCode int, err string) {
+		w.WriteHeader(statusCode)
+		w.Write([]byte(err))
+		return
+	}
+
+	recoveryHandler := func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				if r := recover(); r != nil {
+					writeError(w, http.StatusInternalServerError, fmt.Sprintf("%v", r))
+				}
+			}()
+			h.ServeHTTP(w, r)
+		})
+	}
+
+	startTestServer := func(postSwapsHandler func() http.HandlerFunc, doneCh <-chan struct{}, port int64) {
+		r := mux.NewRouter()
+		r.HandleFunc("/swaps", postSwapsHandler()).Methods("POST")
+		r.Use(recoveryHandler)
+		handler := cors.New(cors.Options{
+			AllowedOrigins:   []string{"*"},
+			AllowCredentials: true,
+			AllowedMethods:   []string{"GET", "POST"},
+		}).Handler(r)
+
+		listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+		if err != nil {
+			panic(err)
+		}
+
+		if err := http.Serve(listener, handler); err != nil {
+			panic(err)
+		}
+	}
+
+	randomString := func() string {
+		id := [32]byte{}
+		rand.Read(id[:])
+		return base64.StdEncoding.EncodeToString(id[:])
+	}
+
+	tokenPairOptions := []struct {
+		sendToken, receiveToken string
+	}{
+		{foundation.TokenBTC.String(), foundation.TokenETH.String()},
+		{foundation.TokenBTC.String(), foundation.TokenWBTC.String()},
+		{foundation.TokenETH.String(), foundation.TokenBTC.String()},
+		{foundation.TokenETH.String(), foundation.TokenWBTC.String()},
+		{foundation.TokenWBTC.String(), foundation.TokenBTC.String()},
+		{foundation.TokenWBTC.String(), foundation.TokenETH.String()},
+	}
+
+	amountOptions := []struct {
+		sendAmount, receiveAmount, minReceiveAmount string
+	}{
+		{"1000", "100", "0"},
+		{"1000", "100", "10"},
+		{"1000", "100", "100"},
+
+		{"100", "1000", "0"},
+		{"100", "1000", "100"},
+		{"100", "1000", "1000"},
+	}
+
+	honestUpdateAmountOptions := []struct {
+		sendAmount, receiveAmount string
+	}{
+		{"800", "60"},
+		{"100", "10"},
+		{"80", "100"},
+
+		{"80", "950"},
+		{"10", "100"},
+		{"8", "1200"},
+	}
+
+	maliciousUpdateAmountOptions := []struct {
+		sendAmount, receiveAmount string
+	}{
+		{"1200", "100"},
+		{"1000", "120"},
+		{"100", "10"},
+
+		{"100", "800"},
+		{"120", "1000"},
+		{"10", "100"},
+	}
+
+	initiationOptions := []bool{
+		true,
+		false,
+	}
+
+	partialSwaps := []foundation.SwapBlob{}
+	for _, tokenPairOption := range tokenPairOptions {
+		for i, amountOption := range amountOptions {
+			for _, initiationOption := range initiationOptions {
+				swap := foundation.SwapBlob{
+					ID:                   foundation.RandomID(),
+					SendToken:            tokenPairOption.sendToken,
+					ReceiveToken:         tokenPairOption.receiveToken,
+					SendAmount:           amountOption.sendAmount,
+					ReceiveAmount:        amountOption.receiveAmount,
+					MinimumReceiveAmount: amountOption.minReceiveAmount,
+					ShouldInitiateFirst:  initiationOption,
+					Delay:                true,
+					DelayInfo:            fmt.Sprintf("%d", i),
+				}
+
+				if initiationOption {
+					swap.SecretHash = randomString()
+					swap.TimeLock = time.Now().Unix()
+				}
+
+				partialSwaps = append(partialSwaps, swap)
+			}
+		}
+	}
+
+	Context("when the broker is not changing amounts being swapped", func() {
+		doneCh := make(chan struct{})
+		go startTestServer(func() http.HandlerFunc {
+			return func(w http.ResponseWriter, r *http.Request) {
+				swap := foundation.SwapBlob{}
+				if err := json.NewDecoder(r.Body).Decode(&swap); err != nil {
+					writeError(w, http.StatusBadRequest, fmt.Sprintf("cannot decode swap request: %v", err))
+					return
+				}
+
+				if !swap.ShouldInitiateFirst {
+					swap.SecretHash = randomString()
+					swap.TimeLock = time.Now().Unix()
+				}
+				swap.SendTo = fmt.Sprintf("Address:%s", swap.SendToken)
+				swap.ReceiveFrom = fmt.Sprintf("Address:%s", swap.ReceiveToken)
+
+				w.WriteHeader(http.StatusOK)
+				if err := json.NewEncoder(w).Encode(swap); err != nil {
+					writeError(w, http.StatusInternalServerError, fmt.Sprintf("cannot encode swap response: %v", err))
+					return
+				}
+			}
+		}, doneCh, 17777)
+
+		for _, pendingSwap := range partialSwaps {
+			It(fmt.Sprintf("verification should succeed, and delay should be set to false"), func() {
+				pendingSwap.DelayCallbackURL = "http://127.0.0.1:17777/swaps"
+				swapFiller := New()
+				filledSwap, err := swapFiller.DelayCallback(pendingSwap)
+				Expect(err).Should(BeNil())
+				Expect(filledSwap.Delay).Should(BeFalse())
+			})
+		}
+
+		close(doneCh)
+	})
+
+	Context("when the broker is changing amounts being swapped honestly", func() {
+		doneCh := make(chan struct{})
+		go startTestServer(func() http.HandlerFunc {
+			return func(w http.ResponseWriter, r *http.Request) {
+				swap := foundation.SwapBlob{}
+				if err := json.NewDecoder(r.Body).Decode(&swap); err != nil {
+					writeError(w, http.StatusBadRequest, fmt.Sprintf("cannot decode swap request: %v", err))
+					return
+				}
+
+				index, err := strconv.ParseInt(swap.DelayInfo, 10, 64)
+				if err != nil {
+					writeError(w, http.StatusBadRequest, fmt.Sprintf("failed to parse delay info: %v", err))
+					return
+				}
+
+				swap.SendAmount = honestUpdateAmountOptions[index].sendAmount
+				swap.ReceiveAmount = honestUpdateAmountOptions[index].receiveAmount
+
+				if !swap.ShouldInitiateFirst {
+					swap.SecretHash = randomString()
+					swap.TimeLock = time.Now().Unix()
+				}
+				swap.SendTo = fmt.Sprintf("Address:%s", swap.SendToken)
+				swap.ReceiveFrom = fmt.Sprintf("Address:%s", swap.ReceiveToken)
+				w.WriteHeader(http.StatusOK)
+				if err := json.NewEncoder(w).Encode(swap); err != nil {
+					writeError(w, http.StatusInternalServerError, fmt.Sprintf("cannot encode swap response: %v", err))
+					return
+				}
+			}
+		}, doneCh, 17778)
+
+		for _, pendingSwap := range partialSwaps {
+			It(fmt.Sprintf("verification should succeed, and delay should be set to false %v", pendingSwap), func() {
+				pendingSwap.DelayCallbackURL = "http://127.0.0.1:17778/swaps"
+				swapFiller := New()
+				filledSwap, err := swapFiller.DelayCallback(pendingSwap)
+				Expect(err).Should(BeNil())
+				Expect(filledSwap.Delay).Should(BeFalse())
+			})
+		}
+		close(doneCh)
+	})
+
+	Context("when the broker is changing amounts being swapped maliciously", func() {
+		doneCh := make(chan struct{})
+		go startTestServer(func() http.HandlerFunc {
+			return func(w http.ResponseWriter, r *http.Request) {
+				swap := foundation.SwapBlob{}
+				if err := json.NewDecoder(r.Body).Decode(&swap); err != nil {
+					writeError(w, http.StatusBadRequest, fmt.Sprintf("cannot decode swap request: %v", err))
+					return
+				}
+
+				index, err := strconv.ParseInt(swap.DelayInfo, 10, 64)
+				if err != nil {
+					writeError(w, http.StatusBadRequest, fmt.Sprintf("failed to parse delay info: %v", err))
+					return
+				}
+
+				swap.SendAmount = maliciousUpdateAmountOptions[index].sendAmount
+				swap.ReceiveAmount = maliciousUpdateAmountOptions[index].receiveAmount
+
+				if !swap.ShouldInitiateFirst {
+					swap.SecretHash = randomString()
+					swap.TimeLock = time.Now().Unix()
+				}
+				swap.SendTo = fmt.Sprintf("Address:%s", swap.SendToken)
+				swap.ReceiveFrom = fmt.Sprintf("Address:%s", swap.ReceiveToken)
+				w.WriteHeader(http.StatusOK)
+				if err := json.NewEncoder(w).Encode(swap); err != nil {
+					writeError(w, http.StatusInternalServerError, fmt.Sprintf("cannot encode swap response: %v", err))
+					return
+				}
+			}
+		}, doneCh, 17779)
+
+		for _, pendingSwap := range partialSwaps {
+			It(fmt.Sprintf("verification should fail %v", pendingSwap), func() {
+				pendingSwap.DelayCallbackURL = "http://127.0.0.1:17779/swaps"
+				swapFiller := New()
+				_, err := swapFiller.DelayCallback(pendingSwap)
+				Expect(err).ShouldNot(BeNil())
+			})
+		}
+		close(doneCh)
+	})
+})
