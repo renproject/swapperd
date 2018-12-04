58c5d1176eb6d00375ce93497bfb09f0f48682da
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-12-04 16:44:41 +1100

    adapter/fund: add contexts with timeout

--- adapter/fund/balance.go
+++ adapter/fund/balance.go
@@ -47,11 +47,13 @@ func (manager *manager) balanceBTC() (foundation.Balance, error) {
 	btcAccount := libbtc.NewAccount(libbtc.NewBlockchainInfoClient(manager.config.Bitcoin.Network.Name), randomKey)
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
 	defer cancel()
+
 	address := manager.config.Bitcoin.Address
 	balance, err := btcAccount.Balance(ctx, address, 0)
 	if err != nil {
 		return foundation.Balance{}, err
 	}
+
 	return foundation.Balance{
 		Address: address,
 		Amount:  big.NewInt(balance).String(),
@@ -64,10 +66,15 @@ func (manager *manager) balanceETH() (foundation.Balance, error) {
 		return foundation.Balance{}, err
 	}
 	address := manager.config.Ethereum.Address
-	balance, err := client.BalanceOf(context.Background(), common.HexToAddress(address))
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+	defer cancel()
+
+	balance, err := client.BalanceOf(ctx, common.HexToAddress(address))
 	if err != nil {
 		return foundation.Balance{}, err
 	}
+
 	return foundation.Balance{
 		Address: address,
 		Amount:  balance.String(),
@@ -88,9 +95,13 @@ func (manager *manager) balanceERC20(token foundation.TokenName) (foundation.Bal
 	if err != nil {
 		return foundation.Balance{}, err
 	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
 	var balance *big.Int
 	if err := client.Get(
-		context.Background(),
+		ctx,
 		func() error {
 			balance, err = erc20Contract.BalanceOf(&bind.CallOpts{}, common.HexToAddress(address))
 			return err
@@ -98,6 +109,7 @@ func (manager *manager) balanceERC20(token foundation.TokenName) (foundation.Bal
 	); err != nil {
 		return foundation.Balance{}, err
 	}
+
 	return foundation.Balance{
 		Address: address,
 		Amount:  balance.String(),
