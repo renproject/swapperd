62642ed1e5a3e2ea52c9cb00e45b822b6ac19ab3
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-12-04 17:59:43 +1100

    adapter/fund: fixed verify balance function

--- adapter/fund/amount.go
+++ adapter/fund/amount.go
@@ -8,7 +8,19 @@ import (
 )
 
 func (manager *manager) VerifyBalance(token foundation.Token, amount *big.Int) error {
-	balance, err := manager.balance(token.Name)
+	switch token.Blockchain {
+	case foundation.Ethereum:
+		return manager.verifyEthereumBalance()
+	case foundation.Bitcoin:
+
+		return manager.verifyBitcoinBalance(amount)
+	default:
+		return foundation.NewErrUnsupportedToken("unsupported blockchain")
+	}
+}
+
+func (manager *manager) verifyEthereumBalance() error {
+	balance, err := manager.balance(foundation.ETH)
 	if err != nil {
 		return err
 	}
@@ -18,30 +30,28 @@ func (manager *manager) VerifyBalance(token foundation.Token, amount *big.Int) e
 		return fmt.Errorf("invalid balance amount %s", balance.Amount)
 	}
 
-	leftover := amount.Sub(balanceAmount, amount)
-	// leftover amount should be greater than the min tx fees
-	switch token.Blockchain {
-	case foundation.Ethereum:
-		return manager.verifyEthereumBalance(leftover)
-	case foundation.Bitcoin:
-		return manager.verifyBitcoinBalance(leftover)
-	default:
-		return foundation.NewErrUnsupportedToken("unsupported blockchain")
-	}
-}
-
-func (manager *manager) verifyEthereumBalance(leftover *big.Int) error {
 	minVal, ok := big.NewInt(0).SetString("5000000000000000", 10) // 0.005 eth
 	if !ok {
 		return fmt.Errorf("invalid minimum value")
 	}
-	if leftover.Cmp(minVal) < 0 {
-		return fmt.Errorf("minimum balance required to start an atomic swap on ethereum blockchain is 0.005 eth (to cover the transaction fees)")
+	if balanceAmount.Cmp(minVal) < 0 {
+		return fmt.Errorf("minimum balance required to start an atomic swap on ethereum blockchain is 0.005 eth (to cover the transaction fees) leftover: %v", balanceAmount)
 	}
 	return nil
 }
 
-func (manager *manager) verifyBitcoinBalance(leftover *big.Int) error {
+func (manager *manager) verifyBitcoinBalance(amount *big.Int) error {
+	balance, err := manager.balance(foundation.BTC)
+	if err != nil {
+		return err
+	}
+
+	balanceAmount, ok := big.NewInt(0).SetString(balance.Amount, 10)
+	if !ok {
+		return fmt.Errorf("invalid balance amount %s", balance.Amount)
+	}
+
+	leftover := balanceAmount.Sub(balanceAmount, amount)
 	if leftover.Cmp(big.NewInt(10000)) < 0 {
 		return fmt.Errorf("minimum send amount for bitcoin is 10000 sat")
 	}
