b0977e6919eef7182316cbeab7a7deedee46b8e3
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-12-04 14:58:54 +1100

    driver/leveldb: updated leveldb to support multiple networks

--- driver/leveldb/leveldb.go
+++ driver/leveldb/leveldb.go
@@ -6,18 +6,18 @@ import (
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
-func NewStore() (*leveldb.DB, error) {
-	return leveldb.OpenFile(buildDBPath(), nil)
+func NewStore(network string) (*leveldb.DB, error) {
+	return leveldb.OpenFile(buildDBPath(network), nil)
 }
 
-func buildDBPath() string {
+func buildDBPath(network string) string {
 	unix := os.Getenv("HOME")
 	if unix != "" {
-		return unix + "/.swapperd/db"
+		return unix + "/.swapperd/db/" + network
 	}
 	windows := os.Getenv("userprofile")
 	if windows != "" {
-		return "C:\\windows\\system32\\config\\systemprofile\\swapperd\\db"
+		return "C:\\windows\\system32\\config\\systemprofile\\swapperd\\db\\" + network
 	}
 	panic("unknown Operating System")
 }
