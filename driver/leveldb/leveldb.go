347a27e970f4ee259179d36961c6eb676859729d
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-12-06 11:59:19 +1100

    driver/leveldb: updated leveldb to accept home dir location

--- driver/leveldb/leveldb.go
+++ driver/leveldb/leveldb.go
@@ -1,23 +1,11 @@
 package leveldb
 
 import (
-	"os"
+	"path"
 
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
-func NewStore(network string) (*leveldb.DB, error) {
-	return leveldb.OpenFile(buildDBPath(network), nil)
-}
-
-func buildDBPath(network string) string {
-	unix := os.Getenv("HOME")
-	if unix != "" {
-		return unix + "/.swapperd/db/" + network
-	}
-	windows := os.Getenv("windir")
-	if windows != "" {
-		return windows + "\\swapperd\\db\\" + network
-	}
-	panic("unknown Operating System")
+func NewStore(homeDir, network string) (*leveldb.DB, error) {
+	return leveldb.OpenFile(path.Join(homeDir, "db", network), nil)
 }
