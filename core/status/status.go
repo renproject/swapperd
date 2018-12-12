d0761d748290ee9f5b0ac1e10842529141d2d692
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-12-12 17:14:32 +1100

    core/status: moved status specific bootloading into the service

--- core/status/status.go
+++ core/status/status.go
@@ -3,26 +3,47 @@ package status
 import (
 	"sync"
 
+	"github.com/republicprotocol/co-go"
+
+	"github.com/sirupsen/logrus"
+
 	"github.com/republicprotocol/swapperd/foundation/swap"
 )
 
 type ReceiptQuery struct {
 	Responder chan<- map[swap.SwapID]swap.SwapReceipt
 }
+
+type Storage interface {
+	Receipts() ([]swap.SwapReceipt, error)
+	PutReceipt(receipt swap.SwapReceipt) error
+	UpdateReceipt(id swap.SwapID, update func(receipt *swap.SwapReceipt)) error
+}
 type Statuses interface {
-	Run(done <-chan struct{}, swaps <-chan swap.SwapReceipt, updates <-chan swap.StatusUpdate, queries <-chan ReceiptQuery)
+	Run(done <-chan struct{}, swaps <-chan swap.SwapReceipt, updates <-chan swap.ReceiptUpdate, queries <-chan ReceiptQuery)
 }
 
 type statuses struct {
 	mu       *sync.RWMutex
 	statuses map[swap.SwapID]swap.SwapReceipt
+	storage  Storage
+	logger   logrus.FieldLogger
 }
 
-func New() Statuses {
-	return &statuses{new(sync.RWMutex), map[swap.SwapID]swap.SwapReceipt{}}
+func New(storage Storage, logger logrus.FieldLogger) Statuses {
+	return &statuses{new(sync.RWMutex), map[swap.SwapID]swap.SwapReceipt{}, storage, logger}
 }
 
-func (statuses *statuses) Run(done <-chan struct{}, receipts <-chan swap.SwapReceipt, updates <-chan swap.StatusUpdate, queries <-chan ReceiptQuery) {
+func (statuses *statuses) Run(done <-chan struct{}, receipts <-chan swap.SwapReceipt, updates <-chan swap.ReceiptUpdate, queries <-chan ReceiptQuery) {
+	// Loading historical swap receipts
+	historicalReceipts, err := statuses.storage.Receipts()
+	if err != nil {
+		statuses.logger.Error(err)
+	}
+	co.ParForAll(historicalReceipts, func(i int) {
+		statuses.set(historicalReceipts[i])
+	})
+
 	for {
 		select {
 		case <-done:
@@ -32,11 +53,21 @@ func (statuses *statuses) Run(done <-chan struct{}, receipts <-chan swap.SwapRec
 				return
 			}
 			statuses.set(receipt)
+			go func() {
+				if err := statuses.storage.PutReceipt(receipt); err != nil {
+					statuses.logger.Error(err)
+				}
+			}()
 		case update, ok := <-updates:
 			if !ok {
 				return
 			}
 			statuses.update(update)
+			go func() {
+				if err := statuses.storage.UpdateReceipt(update.ID, update.Update); err != nil {
+					statuses.logger.Error(err)
+				}
+			}()
 		case query, ok := <-queries:
 			if !ok {
 				return
@@ -64,10 +95,10 @@ func (statuses *statuses) set(status swap.SwapReceipt) {
 	statuses.statuses[status.ID] = status
 }
 
-func (statuses *statuses) update(status swap.StatusUpdate) {
+func (statuses *statuses) update(update swap.ReceiptUpdate) {
 	statuses.mu.Lock()
 	defer statuses.mu.Unlock()
-	statusObj := statuses.statuses[status.ID]
-	statusObj.Status = status.Code
-	statuses.statuses[status.ID] = statusObj
+	receipt := statuses.statuses[update.ID]
+	update.Update(&receipt)
+	statuses.statuses[update.ID] = receipt
 }
