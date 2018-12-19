5decf8fc69a3a97d8b6f1a694fbaee689a2b957e
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-12-19 14:26:43 +1100

    foundation/blockchain: add cost blob to adhere to the swap interface

--- foundation/blockchain/cost.go
+++ foundation/blockchain/cost.go
@@ -4,3 +4,24 @@ import "math/big"
 
 // Cost of an atomic swap
 type Cost map[TokenName]*big.Int
+
+// CostBlob is the json representation of cost.
+type CostBlob map[TokenName]string
+
+// CostToCostBlob converts cost to cost blob
+func CostToCostBlob(cost Cost) CostBlob {
+	costBlob := CostBlob{}
+	for tokenName, tokenCost := range cost {
+		costBlob[tokenName] = tokenCost.String()
+	}
+	return costBlob
+}
+
+// CostBlobToCost converts cost blob to cost
+func CostBlobToCost(costBlob CostBlob) Cost {
+	cost := Cost{}
+	for tokenName, tokenCost := range costBlob {
+		cost[tokenName], _ = new(big.Int).SetString(tokenCost, 10)
+	}
+	return cost
+}
