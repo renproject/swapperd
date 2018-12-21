1f23a35e8c139443bcf3be7fcbf2cfed0e3d910f
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-12-21 15:00:44 +1100

    foundation/blockchain: add new token support

--- foundation/blockchain/token.go
+++ foundation/blockchain/token.go
@@ -30,12 +30,22 @@ var (
 	BTC  = TokenName("BTC")
 	ETH  = TokenName("ETH")
 	WBTC = TokenName("WBTC")
+	REN  = TokenName("REN")
+	TUSD = TokenName("TUSD")
+	DGX  = TokenName("DGX")
+	ZRX  = TokenName("ZRX")
+	OMG  = TokenName("OMG")
 )
 
 var (
 	TokenBTC  = Token{TokenName("BTC"), Bitcoin}
 	TokenETH  = Token{TokenName("ETH"), Ethereum}
 	TokenWBTC = Token{TokenName("WBTC"), Ethereum}
+	TokenREN  = Token{TokenName("REN"), Ethereum}
+	TokenTUSD = Token{TokenName("TUSD"), Ethereum}
+	TokenDGX  = Token{TokenName("DGX"), Ethereum}
+	TokenZRX  = Token{TokenName("ZRX"), Ethereum}
+	TokenOMG  = Token{TokenName("OMG"), Ethereum}
 )
 
 func PatchToken(token string) (Token, error) {
@@ -47,6 +57,16 @@ func PatchToken(token string) (Token, error) {
 		return TokenWBTC, nil
 	case "ethereum", "eth", "ether":
 		return TokenETH, nil
+	case "ren", "republictoken", "republic token":
+		return TokenREN, nil
+	case "tusd", "trueusd", "true usd":
+		return TokenTUSD, nil
+	case "digix gold token", "dgx", "dgt":
+		return TokenDGX, nil
+	case "zerox", "zrx", "0x":
+		return TokenZRX, nil
+	case "omisego", "omg", "omise go":
+		return TokenOMG, nil
 	default:
 		return Token{}, fmt.Errorf("unsupported token: %s", token)
 	}
