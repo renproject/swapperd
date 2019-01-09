3094db3a95cf48fb831e66547479bea56f520412
Susruth  Nadimpalli <susruth@republicprotocol.com>
2019-01-09 17:44:27 +1100

    adapter/wallet: updated wallet to expect passwords

--- adapter/wallet/wallet.go
+++ adapter/wallet/wallet.go
@@ -5,8 +5,7 @@ import (
 
 	"github.com/republicprotocol/beth-go"
 	"github.com/republicprotocol/libbtc-go"
-	"github.com/republicprotocol/swapperd/core/wallet/balance"
-	"github.com/republicprotocol/swapperd/core/wallet/transfer"
+	"github.com/republicprotocol/swapperd/core/transfer"
 	"github.com/republicprotocol/swapperd/foundation/blockchain"
 )
 
@@ -34,19 +33,16 @@ type Balance struct {
 }
 
 type Wallet interface {
-	ID() string
+	ID(password string) (string, error)
 	SupportedTokens() []blockchain.Token
 	SupportedBlockchains() []blockchain.Blockchain
-	Balances() (balance.BalanceMap, error)
-	BalancesWithPassword(password string) (balance.BalanceMap, error)
+	Balances(password string) (map[blockchain.TokenName]blockchain.Balance, error)
 	Lookup(token blockchain.Token, txHash string) (transfer.UpdateReceipt, error)
 	Transfer(password string, token blockchain.Token, to string, amount *big.Int) (string, error)
-	GetAddress(blockchain blockchain.BlockchainName) (string, error)
-	GetAddressWithPassword(blockchainName blockchain.BlockchainName, password string) (string, error)
-	Addresses() (map[blockchain.TokenName]string, error)
-	AddressesWithPassword(password string) (map[blockchain.TokenName]string, error)
+	GetAddress(password string, blockchainName blockchain.BlockchainName) (string, error)
+	Addresses(password string) (map[blockchain.TokenName]string, error)
 	VerifyAddress(blockchain blockchain.BlockchainName, address string) error
-	VerifyBalance(token blockchain.Token, balance *big.Int) error
+	VerifyBalance(password string, token blockchain.Token, balance *big.Int) error
 	DefaultFee(blockchainName blockchain.BlockchainName) (*big.Int, error)
 
 	EthereumAccount(password string) (beth.Account, error)
