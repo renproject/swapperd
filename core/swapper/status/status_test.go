c87025f667e0bdf30cbd06dee0e2af2de8840dee
Susruth  Nadimpalli <susruth@republicprotocol.com>
2019-01-15 13:12:33 +1100

    core/swapper/status: updated status test to work with tau tasks

--- core/swapper/status/status_test.go
+++ core/swapper/status/status_test.go
@@ -6,13 +6,15 @@ import (
 	"testing/quick"
 	"time"
 
+	"github.com/republicprotocol/swapperd/testutils"
+
+	"github.com/republicprotocol/tau"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	. "github.com/republicprotocol/swapperd/core/swapper/status"
 
-	. "github.com/republicprotocol/swapperd/core/status"
-	"github.com/republicprotocol/swapperd/driver/logger"
 	"github.com/republicprotocol/swapperd/foundation/swap"
-	"github.com/republicprotocol/swapperd/testutils"
 )
 
 var Random *rand.Rand
@@ -21,104 +23,74 @@ func init() {
 	Random = rand.New(rand.NewSource(time.Now().Unix()))
 }
 
-var _ = Describe("Statuses", func() {
+var _ = Describe("Status Task", func() {
 
-	init := func() (Statuses, chan struct{}, chan swap.SwapReceipt, chan swap.ReceiptUpdate, chan ReceiptQuery) {
-		logger := logger.NewStdOut()
-		store := testutils.NewMockStorage()
-		statuses := New(store, logger)
-		done := make(chan struct{})
-		receipts := make(chan swap.SwapReceipt)
-		updates := make(chan swap.ReceiptUpdate)
-		queries := make(chan ReceiptQuery)
-
-		return statuses, done, receipts, updates, queries
+	init := func() (tau.Task, chan struct{}) {
+		return New(128), make(chan struct{})
 	}
 
 	Context("when receiving new receipt", func() {
 		It("should store the receipt", func() {
-			statuses, done, receipts, updates, queries := init()
+			statusTask, done := init()
 			defer close(done)
-			go statuses.Run(done, receipts, updates, queries)
+			go statusTask.Run(done)
 
-			test := func(receipt swap.SwapReceipt) bool {
-				receipts <- receipt
+			test := func(receipt Receipt) bool {
+				statusTask.IO().InputWriter() <- receipt
 				responder := make(chan map[swap.SwapID]swap.SwapReceipt, 1)
 				query := ReceiptQuery{
 					Responder: responder,
 				}
-				queries <- query
-
+				statusTask.IO().InputWriter() <- query
 				response := <-responder
-				return reflect.DeepEqual(response[receipt.ID], receipt)
+				return reflect.DeepEqual(response[receipt.ID], swap.SwapReceipt(receipt))
 			}
 
-			Expect(quick.Check(test, nil)).ShouldNot(HaveOccurred())
+			Expect(quick.Check(test, testutils.DefaultQuickCheckConfig)).ShouldNot(HaveOccurred())
 		})
 	})
 
 	Context("when receiving new update", func() {
 		It("should update the receipt", func() {
-			statuses, done, receipts, updates, queries := init()
+			statusTask, done := init()
 			defer close(done)
-			go statuses.Run(done, receipts, updates, queries)
+			go statusTask.Run(done)
 
-			test := func(receipt swap.SwapReceipt) bool {
-				receipts <- receipt
+			test := func(receipt Receipt) bool {
+				statusTask.IO().InputWriter() <- receipt
 
 				receipt.Status = swap.Audited
-				update := swap.NewReceiptUpdate(receipt.ID, func(re *swap.SwapReceipt) {
+				update := ReceiptUpdate(swap.NewReceiptUpdate(receipt.ID, func(re *swap.SwapReceipt) {
 					re.ID = receipt.ID
 					re.Status = swap.Audited
-				})
+				}))
 
-				updates <- update
+				statusTask.IO().InputWriter() <- update
 
 				responder := make(chan map[swap.SwapID]swap.SwapReceipt, 1)
 				query := ReceiptQuery{
 					Responder: responder,
 				}
-				queries <- query
+				statusTask.IO().InputWriter() <- query
 				response := <-responder
 
-				return reflect.DeepEqual(response[receipt.ID], receipt)
+				return reflect.DeepEqual(response[receipt.ID], swap.SwapReceipt(receipt))
 			}
 
-			Expect(quick.Check(test, nil)).ShouldNot(HaveOccurred())
+			Expect(quick.Check(test, testutils.DefaultQuickCheckConfig)).ShouldNot(HaveOccurred())
 		})
 	})
 
-	Context("when closing one of the input channel", func() {
-		It("should stop the statues from running ", func() {
-			statuses, done, receipts, updates, queries := init()
-			go statuses.Run(done, receipts, updates, queries)
+	Context("when receiving an unknown message type", func() {
+		It("should return an error", func() {
+			statusTask, done := init()
+			defer close(done)
+			go statusTask.Run(done)
 
-			close(receipts)
-
-			// Expect(receipts).ShouldNot(BeSent(swap.SwapReceipt{}))
-			Expect(updates).ShouldNot(BeSent(swap.ReceiptUpdate{}))
-			Expect(queries).ShouldNot(BeSent(ReceiptQuery{}))
-		})
-
-		It("should stop the statues from running ", func() {
-			statuses, done, receipts, updates, queries := init()
-			go statuses.Run(done, receipts, updates, queries)
-
-			close(updates)
-			time.Sleep(1 * time.Millisecond)
-
-			Expect(receipts).ShouldNot(BeSent(swap.SwapReceipt{}))
-			Expect(queries).ShouldNot(BeSent(ReceiptQuery{}))
-		})
-
-		It("should stop the statues from running ", func() {
-			statuses, done, receipts, updates, queries := init()
-			go statuses.Run(done, receipts, updates, queries)
-
-			close(queries)
-
-			Expect(receipts).ShouldNot(BeSent(swap.SwapReceipt{}))
-			Expect(updates).ShouldNot(BeSent(swap.ReceiptUpdate{}))
+			statusTask.IO().InputWriter() <- tau.RandomMessage{}
+			err := <-statusTask.IO().OutputReader()
+			_, ok := err.(tau.Error)
+			Expect(ok).Should(BeTrue())
 		})
 	})
 })
