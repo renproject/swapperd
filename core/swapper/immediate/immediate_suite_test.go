1a7d1f93d889a39a85a987d851be4768f8710b98
Susruth  Nadimpalli <susruth@republicprotocol.com>
2019-01-16 15:14:49 +1100

    core/swapper/immediate: add tests for immediate swapper

--- core/swapper/immediate/immediate_suite_test.go
+++ core/swapper/immediate/immediate_suite_test.go
@@ -1,1 +1,103 @@
 package immediate_test
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/republicprotocol/swapperd/core/swapper/immediate"
+	"github.com/republicprotocol/swapperd/foundation/blockchain"
+	"github.com/republicprotocol/swapperd/foundation/swap"
+)
+
+func TestImmediate(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Immediate Suite")
+}
+
+type MockContractBuilder struct {
+}
+
+func NewMockContractBuilder() *MockContractBuilder {
+	return &MockContractBuilder{}
+}
+
+func (builder MockContractBuilder) BuildSwapContracts(request immediate.SwapRequest) (immediate.Contract, immediate.Contract, error) {
+	if uint64(request.Blob.TimeLock)%36 == 8 {
+		return nil, nil, fmt.Errorf("invalid swap object")
+	}
+	return NewMockContract(request.Blob, request.SendCost), NewMockContract(request.Blob, request.ReceiveCost), nil
+}
+
+type MockContract struct {
+	rand   *rand.Rand
+	blob   swap.SwapBlob
+	cost   blockchain.Cost
+	status int
+}
+
+func NewMockContract(blob swap.SwapBlob, cost blockchain.Cost) *MockContract {
+	return &MockContract{
+		blob:   blob,
+		cost:   cost,
+		status: 0,
+	}
+}
+
+func (contract *MockContract) Initiate() error {
+	contract.status = swap.Initiated
+	if uint64(contract.blob.TimeLock)%9 == 0 {
+		return fmt.Errorf("Connection Failed")
+	}
+	return nil
+}
+
+func (contract *MockContract) Audit() error {
+	contract.status = swap.Audited
+	if uint64(contract.blob.TimeLock)%9 == 1 {
+		return immediate.ErrSwapExpired
+	}
+	if uint64(contract.blob.TimeLock)%9 == 2 {
+		return immediate.ErrAuditPending
+	}
+	if uint64(contract.blob.TimeLock)%9 == 3 {
+		return fmt.Errorf("Connection Failed")
+	}
+	return nil
+}
+
+func (contract *MockContract) Redeem([32]byte) error {
+	contract.status = swap.Redeemed
+	if uint64(contract.blob.TimeLock)%9 == 4 {
+		return fmt.Errorf("Connection Failed")
+	}
+	return nil
+}
+
+func (contract *MockContract) AuditSecret() ([32]byte, error) {
+	if uint64(contract.blob.TimeLock)%9 == 5 {
+		return [32]byte{}, fmt.Errorf("Connection Failed")
+	}
+	if uint64(contract.blob.TimeLock)%9 == 6 {
+		return [32]byte{}, immediate.ErrAuditPending
+	}
+	if uint64(contract.blob.TimeLock)%9 == 7 {
+		return [32]byte{}, immediate.ErrSwapExpired
+	}
+	return [32]byte{}, nil
+}
+
+func (contract *MockContract) Refund() error {
+	contract.status = swap.Refunded
+	if uint64(contract.blob.TimeLock)%18 == 7 || uint64(contract.blob.TimeLock)%18 == 1 {
+		return fmt.Errorf("Connection Failed")
+	}
+	return nil
+}
+
+func (contract *MockContract) Cost() blockchain.Cost {
+	return contract.cost
+}
--- core/swapper/immediate/immediate_test.go
+++ core/swapper/immediate/immediate_test.go
@@ -1,1 +1,138 @@
 package immediate_test
+
+import (
+	"testing/quick"
+
+	"github.com/republicprotocol/swapperd/foundation/blockchain"
+	"github.com/republicprotocol/swapperd/foundation/swap"
+	"github.com/republicprotocol/swapperd/testutils"
+	"github.com/republicprotocol/tau"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/republicprotocol/swapperd/core/swapper/immediate"
+)
+
+var _ = Describe("Immediate Swap Task", func() {
+
+	init := func() (tau.Task, chan struct{}) {
+		contractBuilder := NewMockContractBuilder()
+		return New(1024, contractBuilder), make(chan struct{})
+	}
+
+	handleSwapResponse := func(msg tau.Message, blob swap.SwapBlob) bool {
+		timeLock := uint64(blob.TimeLock)
+		if timeLock%36 == 8 {
+			_, ok := msg.(tau.Error)
+			return ok
+		}
+
+		messages := msg.(tau.MessageBatch)
+		update := messages[0].(ReceiptUpdate)
+		receipt := swap.NewSwapReceipt(blob)
+		update.Update(&receipt)
+
+		switch timeLock % 9 {
+		case 0:
+			_, ok := messages[1].(tau.Error)
+			if blob.ShouldInitiateFirst {
+				return ok && len(messages) == 2 && receipt.Status == swap.Inactive
+			}
+			return ok && len(messages) == 2 && receipt.Status == swap.Audited
+		case 1:
+			if !blob.ShouldInitiateFirst {
+				_, msg1ok := messages[1].(tau.Error)
+				deleteSwap, msg2ok := messages[2].(DeleteSwap)
+				return msg1ok && msg2ok && len(messages) == 3 && receipt.Status == swap.Expired && deleteSwap.ID == blob.ID
+			}
+			if timeLock%18 == 1 {
+				_, ok := messages[1].(tau.Error)
+				return ok && len(messages) == 2 && receipt.Status == swap.RefundFailed
+			}
+			deleteSwap, ok := messages[1].(DeleteSwap)
+			return ok && len(messages) == 2 && deleteSwap.ID == blob.ID && receipt.Status == swap.Refunded
+		case 2:
+			return len(messages) == 1
+		case 3:
+			_, ok := messages[1].(tau.Error)
+			return ok && len(messages) == 2 && receipt.Status == swap.AuditPending
+		case 4:
+			_, ok := messages[1].(tau.Error)
+			if blob.ShouldInitiateFirst {
+				return ok && len(messages) == 2 && receipt.Status == swap.Audited
+			}
+			return ok && len(messages) == 2 && receipt.Status == swap.AuditedSecret
+		case 5:
+			_, ok := messages[1].(tau.Error)
+			return blob.ShouldInitiateFirst || ok && len(messages) == 2 && receipt.Status == swap.Initiated
+		case 6:
+			return blob.ShouldInitiateFirst || len(messages) == 1
+		case 7:
+			if timeLock%18 == 7 {
+				_, ok := messages[1].(tau.Error)
+				return blob.ShouldInitiateFirst || ok && len(messages) == 2 && receipt.Status == swap.RefundFailed
+			}
+			deleteSwap, ok := messages[1].(DeleteSwap)
+			return blob.ShouldInitiateFirst || ok && len(messages) == 2 && deleteSwap.ID == blob.ID && receipt.Status == swap.Refunded
+		case 8:
+			deleteSwap, ok := messages[1].(DeleteSwap)
+			return ok && len(messages) == 2 && receipt.Status == swap.Redeemed && deleteSwap.ID == blob.ID
+		default:
+			return false
+		}
+	}
+
+	Context("when receiving new immediate swap request", func() {
+		It("should return receipt update and new swap on nil error", func() {
+			immediateTask, done := init()
+			defer close(done)
+			go immediateTask.Run(done)
+
+			test := func(blob swap.SwapBlob) bool {
+				request := NewSwapRequest(blob, blockchain.Cost{}, blockchain.Cost{})
+				immediateTask.IO().InputWriter() <- request
+				response := <-immediateTask.IO().OutputReader()
+
+				return handleSwapResponse(response, blob)
+			}
+
+			Expect(quick.Check(test, testutils.DefaultQuickCheckConfig)).ShouldNot(HaveOccurred())
+		})
+
+		Context("when receiveng new tick", func() {
+			It("should return ", func() {
+				immediateTask, done := init()
+				defer close(done)
+				go immediateTask.Run(done)
+
+				test := func(blob swap.SwapBlob) bool {
+					request := NewSwapRequest(blob, blockchain.Cost{}, blockchain.Cost{})
+					immediateTask.IO().InputWriter() <- request
+					<-immediateTask.IO().OutputReader()
+
+					immediateTask.IO().InputWriter() <- tau.Tick{}
+					return true
+				}
+
+				Expect(quick.Check(test, testutils.DefaultQuickCheckConfig)).ShouldNot(HaveOccurred())
+			})
+		})
+
+		Context("when receiving an unknown message type", func() {
+			It("should return an error", func() {
+				immediateTask, done := init()
+				defer close(done)
+				go immediateTask.Run(done)
+
+				test := func() bool {
+					immediateTask.IO().InputWriter() <- tau.RandomMessage{}
+					err := <-immediateTask.IO().OutputReader()
+					_, ok := err.(tau.Error)
+					return ok
+				}
+
+				Expect(quick.Check(test, testutils.DefaultQuickCheckConfig)).ShouldNot(HaveOccurred())
+			})
+		})
+	})
+})
