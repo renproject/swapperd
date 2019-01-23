e74c18c82c52418f879faac6558b7ef7baaec773
Susruth  Nadimpalli <susruth@republicprotocol.com>
2019-01-24 09:49:52 +1100

    adapter/binder/erc20: fixed erc20 swap cost calcualtion

--- adapter/binder/erc20/erc20.go
+++ adapter/binder/erc20/erc20.go
@@ -184,6 +184,10 @@ func (atom *erc20SwapContractBinder) Refund() error {
 			txFee := new(big.Int).Mul(tx.GasPrice(), big.NewInt(int64(tx.Gas())))
 			atom.cost[blockchain.ETH] = new(big.Int).Add(atom.cost[blockchain.ETH], txFee)
 
+			if _, ok := atom.cost[atom.swap.Token.Name]; ok {
+				atom.cost[atom.swap.Token.Name] = new(big.Int).Sub(atom.cost[atom.swap.Token.Name], atom.swap.BrokerFee)
+			}
+
 			msg, _ := atom.account.FormatTransactionView("Refunded on Ethereum blockchain", tx.Hash().String())
 			atom.logger.Info(msg)
 			return tx, nil
