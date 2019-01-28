d2bdefda2bdfeadb7555a69392328cce780226ff
Susruth  Nadimpalli <susruth@republicprotocol.com>
2019-01-28 18:03:31 +1100

    driver/composer: updated to use tau framework

--- driver/composer/composer.go
+++ driver/composer/composer.go
@@ -1,26 +1,27 @@
 package composer
 
 import (
+	"time"
+
+	"github.com/republicprotocol/co-go"
 	"github.com/republicprotocol/swapperd/adapter/binder"
 	"github.com/republicprotocol/swapperd/adapter/callback"
 	"github.com/republicprotocol/swapperd/adapter/db"
 	"github.com/republicprotocol/swapperd/adapter/server"
-	"github.com/republicprotocol/swapperd/core/swapper"
-	"github.com/republicprotocol/swapperd/core/swapper/delayed"
-	"github.com/republicprotocol/swapperd/core/swapper/immediate"
-	"github.com/republicprotocol/swapperd/core/swapper/status"
-	"github.com/republicprotocol/swapperd/core/transfer"
+	"github.com/republicprotocol/swapperd/core/wallet"
 	"github.com/republicprotocol/swapperd/driver/keystore"
 	"github.com/republicprotocol/swapperd/driver/leveldb"
 	"github.com/republicprotocol/swapperd/driver/logger"
+	"github.com/republicprotocol/tau"
+	"github.com/sirupsen/logrus"
 )
 
-const BufferCapacity = 128
+const BufferCapacity = 2048
 
 type composer struct {
-	homeDir string
-	network string
-	port    string
+	server     server.Server
+	logger     logrus.FieldLogger
+	walletTask tau.Task
 }
 
 type Composer interface {
@@ -28,16 +29,7 @@ type Composer interface {
 }
 
 func New(homeDir, network, port string) Composer {
-	return &composer{homeDir, network, port}
-}
-
-func (composer *composer) Run(done <-chan struct{}) {
-	blockchain, err := keystore.Wallet(composer.homeDir, composer.network)
-	if err != nil {
-		panic(err)
-	}
-
-	ldb, err := leveldb.NewStore(composer.homeDir, composer.network)
+	ldb, err := leveldb.NewStore(homeDir, network)
 	if err != nil {
 		panic(err)
 	}
@@ -45,13 +37,55 @@ func (composer *composer) Run(done <-chan struct{}) {
 	storage := db.New(ldb)
 	logger := logger.NewStdOut()
 
-	delayedSwapperTask := delayed.New(BufferCapacity, callback.New())
-	immediateSwapperTask := immediate.New(BufferCapacity, binder.NewBuilder(blockchain, logger))
-	swapStatusTask := status.New(BufferCapacity)
+	bc, err := keystore.Wallet(homeDir, network)
+	if err != nil {
+		panic(err)
+	}
+	server := server.NewHttpServer(BufferCapacity, port, bc, logger)
 
-	swapperTask := swapper.New(BufferCapacity, storage, delayedSwapperTask, immediateSwapperTask, swapStatusTask)
-	walletTask := transfer.New(BufferCapacity, blockchain, storage)
+	walletTask := wallet.New(BufferCapacity, storage, bc, binder.NewBuilder(bc, logger), callback.New())
+	return &composer{server, logger, walletTask}
+}
 
-	httpServer := server.NewHttpServer(blockchain, logger, swapperTask, walletTask, composer.port)
-	httpServer.Run(done)
+func (composer *composer) Run(done <-chan struct{}) {
+	io := tau.NewIO(BufferCapacity)
+	co.ParBegin(
+		func() {
+			io.InputWriter() <- tau.NewTick(time.Now())
+		},
+		func() {
+			tau.New(io, tau.ReduceFunc(func(msg tau.Message) tau.Message {
+				switch msg := msg.(type) {
+				case tau.Tick:
+					composer.walletTask.Send(msg)
+				case wallet.AcceptRequest:
+					composer.handleAcceptRequest()
+				case tau.Error:
+					composer.logger.Error(msg)
+				default:
+					composer.logger.Errorf("Unexpected message type: %T in compser", msg)
+				}
+				return nil
+			}), composer.walletTask).Run(done)
+		},
+		func() {
+			composer.server.Run(done)
+		},
+	)
+}
+
+func (composer *composer) handleAcceptRequest() {
+	msg, err := composer.server.Receive()
+	if err != nil {
+		composer.logger.Error(err)
+		return
+	}
+
+	switch msg := msg.(type) {
+	case tau.Tick, wallet.TransferRequest, wallet.SwapperRequest:
+		composer.walletTask.Send(msg)
+	default:
+		composer.logger.Errorf("Unexpected message type: %T from server", msg)
+	}
+	return
 }
