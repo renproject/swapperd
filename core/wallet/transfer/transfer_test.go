21457426de5d43c26154ffc4a7d66c70caef1a31
Susruth  Nadimpalli <susruth@republicprotocol.com>
2019-02-05 14:41:05 +1100

    core/wallet/transfer: updated transfer tests

--- core/wallet/transfer/transfer_test.go
+++ core/wallet/transfer/transfer_test.go
@@ -5,7 +5,8 @@ import (
 	"testing/quick"
 	"time"
 
-	"github.com/renproject/swapperd/foundation/swap"
+	"github.com/renproject/swapperd/foundation/blockchain"
+	"github.com/renproject/swapperd/testutils"
 	"github.com/republicprotocol/tau"
 
 	. "github.com/onsi/ginkgo"
@@ -13,7 +14,7 @@ import (
 	. "github.com/renproject/swapperd/core/wallet/transfer"
 )
 
-const BufferLimit = 1024
+const BufferCapacity = 1024
 
 var DefaultQuickCheckConfig = &quick.Config{
 	Rand:     rand.New(rand.New(rand.NewSource(time.Now().Unix()))),
@@ -21,70 +22,21 @@ var DefaultQuickCheckConfig = &quick.Config{
 }
 
 var _ = Describe("Transfer Task", func() {
-
-	responseHandler := func(reducer tau.ReduceFunc) (tau.Task, chan struct{}) {
-		storage := NewMockStorage()
-		bc := NewMockBlockhain()
-		doneCh := make(chan struct{})
-		transferTask := New(BufferLimit, bc, storage)
-		go tau.New(tau.NewIO(BufferLimit), reducer).Run(doneCh)
-		return transferTask, doneCh
-	}
-
-	Context("when receiving a bootload request", func() {
-		It("should return an error or bootload properly", func() {
-			transferTask, doneCh := responseHandler(func(msg tau.Message) tau.Message {
-				switch msg := msg.(type) {
-				case tau.Error:
-					return nil
-				default:
-					Expect(msg).ShouldNot(HaveOccurred())
-					return nil
-				}
-			})
-			defer close(doneCh)
-
-			test := func(blob swap.SwapBlob) bool {
-				transferTask.IO().InputWriter() <- Bootload{}
-				return true
+	Context("when receiving an unknown message type", func() {
+		It("should return an error", func() {
+			bc := testutils.NewMockBlockchain(map[blockchain.TokenName]blockchain.Balance{})
+			storage := testutils.NewMockStorage()
+			transferTask := New(BufferCapacity, bc, storage)
+			done := make(chan struct{})
+			defer close(done)
+			go transferTask.Run(done)
+			test := func() bool {
+				transferTask.IO().InputWriter() <- tau.RandomMessage{}
+				err := <-transferTask.IO().OutputReader()
+				_, ok := err.(tau.Error)
+				return ok
 			}
-			Expect(quick.Check(test, DefaultQuickCheckConfig)).ShouldNot(HaveOccurred())
+			Expect(quick.Check(test, testutils.DefaultQuickCheckConfig)).ShouldNot(HaveOccurred())
 		})
-
-		// Context("when receiveng new tick", func() {
-		// 	It("should return ", func() {
-		// 		immediateTask, done := init()
-		// 		defer close(done)
-		// 		go immediateTask.Run(done)
-
-		// 		test := func(blob swap.SwapBlob) bool {
-		// 			request := NewSwapRequest(blob, blockchain.Cost{}, blockchain.Cost{})
-		// 			immediateTask.IO().InputWriter() <- request
-		// 			<-immediateTask.IO().OutputReader()
-		// 			immediateTask.IO().InputWriter() <- tau.Tick{}
-		// 			<-immediateTask.IO().OutputReader()
-		// 			return true
-		// 		}
-
-		// 		Expect(quick.Check(test, testutils.DefaultQuickCheckConfig)).ShouldNot(HaveOccurred())
-		// 	})
-		// })
-
-		// Context("when receiving an unknown message type", func() {
-		// 	It("should return an error", func() {
-		// 		immediateTask, done := init()
-		// 		defer close(done)
-		// 		go immediateTask.Run(done)
-
-		// 		test := func() bool {
-		// 			immediateTask.IO().InputWriter() <- tau.RandomMessage{}
-		// 			err := <-immediateTask.IO().OutputReader()
-		// 			_, ok := err.(tau.Error)
-		// 			return ok
-		// 		}
-
-		// 		Expect(quick.Check(test, testutils.DefaultQuickCheckConfig)).ShouldNot(HaveOccurred())
-		// 	})
-		// })
 	})
 })
