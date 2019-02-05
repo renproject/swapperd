c322aa6f5af0983e0cd22ff81770f4bbf8712b5e
Susruth  Nadimpalli <susruth@republicprotocol.com>
2019-02-05 14:39:32 +1100

    core/wallet/status: moved mock storage to testutils

--- core/wallet/status/status_suite_test.go
+++ core/wallet/status/status_suite_test.go
@@ -1,12 +1,8 @@
 package status_test
 
 import (
-	"errors"
-	"sync"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/renproject/swapperd/foundation/swap"
 
 	"testing"
 )
@@ -14,45 +10,4 @@ import (
 func TestStatus(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Status Suite")
-}
-
-type MockStorage struct {
-	mu       *sync.RWMutex
-	receipts map[swap.SwapID]swap.SwapReceipt
-}
-
-func NewMockStorage() *MockStorage {
-	return &MockStorage{
-		mu:       new(sync.RWMutex),
-		receipts: map[swap.SwapID]swap.SwapReceipt{},
-	}
-}
-
-func (store *MockStorage) Receipt(id swap.SwapID) (swap.SwapReceipt, error) {
-	store.mu.RLock()
-	defer store.mu.RUnlock()
-
-	return store.receipts[id], nil
-}
-
-func (store *MockStorage) PutReceipt(receipt swap.SwapReceipt) error {
-	store.mu.Lock()
-	defer store.mu.Unlock()
-
-	store.receipts[receipt.ID] = receipt
-	return nil
-}
-
-func (store *MockStorage) UpdateReceipt(update swap.ReceiptUpdate) error {
-	store.mu.Lock()
-	defer store.mu.Unlock()
-
-	receipt, ok := store.receipts[update.ID]
-	if !ok {
-		return errors.New("swap not found")
-	}
-
-	update.Update(&receipt)
-	store.receipts[update.ID] = receipt
-	return nil
 }
