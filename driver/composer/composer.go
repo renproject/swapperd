1b403c76175290e875cc9694669be836961deae5
Susruth  Nadimpalli <susruth@republicprotocol.com>
2019-02-14 11:16:45 +1100

    driver/composer: updated composer to use fs notifier

--- driver/composer/composer.go
+++ driver/composer/composer.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/renproject/swapperd/driver/notifier"
 	"github.com/renproject/swapperd/driver/swapperd"
 	"github.com/renproject/swapperd/driver/updater"
 	"github.com/republicprotocol/co-go"
@@ -37,10 +38,9 @@ func Run(done <-chan struct{}) {
 		panic(err)
 	}
 
-	version := make(chan string, 1)
 	co.ParBegin(
 		func() {
-			updater.New(config.Version, homeDir, config.Frequency*time.Second, logger).Run(done, version)
+			updater.New(config.Version, homeDir, config.Frequency*time.Second, logger).Run(done)
 		},
 		func() {
 			swapperd.New(config.Version, homeDir, "testnet", "17927", logger).Run(done)
@@ -49,21 +49,7 @@ func Run(done <-chan struct{}) {
 			swapperd.New(config.Version, homeDir, "mainnet", "7927", logger).Run(done)
 		},
 		func() {
-			select {
-			case <-done:
-			case ver := <-version:
-				config.Version = ver
-				configBytes, err := json.Marshal(config)
-				if err != nil {
-					logger.Println(err)
-					return
-				}
-				if err := ioutil.WriteFile(fmt.Sprintf("%s/config.json", homeDir), configBytes, 0644); err != nil {
-					logger.Println(err)
-					return
-				}
-				os.Exit(0)
-			}
+			notifier.New(homeDir, logger).Watch(done, "config.json", "mainnet.json", "testnet.json")
 		},
 	)
 }
