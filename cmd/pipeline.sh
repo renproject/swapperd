837d86c1d4735b719703e637fd0fb66589bfdea1
Susruth  Nadimpalli <susruth@republicprotocol.com>
2019-02-25 17:35:19 +1100

    Add updater binaries to pipeline

--- cmd/pipeline.sh
+++ cmd/pipeline.sh
@@ -5,6 +5,10 @@ cd installer
 go build .
 xgo --targets=linux/amd64,windows/amd64 .
 
+cd ../updater/
+go build .
+xgo --targets=linux/amd64,windows/amd64 .
+
 cd ../swapperd-unix/
 go build .
 xgo --targets=linux/amd64 .
@@ -23,12 +27,14 @@ xgo --targets=linux/amd64,windows/amd64 .
 
 cd ../
 mv installer/installer bin/installer
+mv updater/updater bin/updater
 mv swapperd-unix/swapperd-unix bin/swapperd
 mv updater-unix/updater-unix bin/swapperd-updater
 mv uninstaller/uninstaller bin/uninstaller
 zip -r swapper_darwin_amd64.zip bin
 
 mv installer/installer-linux-amd64 bin/installer
+mv updater/updater-linux-amd64 bin/updater
 mv swapperd-unix/swapperd-unix-linux-amd64 bin/swapperd
 mv updater-unix/updater-unix-linux-amd64 bin/swapperd-updater
 mv uninstaller/uninstaller-linux-amd64 bin/uninstaller
@@ -37,6 +43,7 @@ rm -rf bin
 
 mkdir -p bin
 mv installer/installer-windows-4.0-amd64.exe bin/installer.exe
+mv updater/updater-windows-4.0-amd64.exe bin/updater.exe
 mv swapperd-win/swapperd-win-windows-4.0-amd64.exe bin/swapperd.exe 
 mv updater-win/updater-win-windows-4.0-amd64.exe bin/swapperd-updater.exe 
 mv uninstaller/uninstaller-windows-4.0-amd64.exe bin/uninstaller.exe 
